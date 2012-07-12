b355db2a2700194cd8475402b000b1ef8c6207af
Chris Sinchok <csinchok@theonion.com>
2012-07-12 12:10:26 -0500

    mdown rename

--- afns/apps/mdown/templatetags/markdown.py
+++ afns/apps/mdown/templatetags/markdown.py
@@ -8,9 +8,9 @@ register = template.Library()
 @register.filter(is_safe=True)
 def markdown(value, arg=''):
     try:
-        import markdown
+        import markdown as markdown_lib
     except ImportError:
         if settings.DEBUG:
             raise template.TemplateSyntaxError("Error in 'markdown' filter: The Python markdown library isn't installed.")
         return force_unicode(value)
-    return mark_safe(markdown.markdown(force_unicode(value), ['smartypants','onion']), safe_mode=False)
+    return mark_safe(markdown_lib.markdown(force_unicode(value), ['smartypants','onion']), safe_mode=False)
