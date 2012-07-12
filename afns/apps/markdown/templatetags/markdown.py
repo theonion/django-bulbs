47e313114964cf92d42d0765e4cc2b0562262206
Chris Sinchok <csinchok@theonion.com>
2012-07-12 11:21:52 -0500

    I trust that I'll have the right markdown lib installed

--- afns/apps/markdown/templatetags/markdown.py
+++ afns/apps/markdown/templatetags/markdown.py
@@ -16,12 +16,4 @@ def markdown(value, arg=''):
             raise template.TemplateSyntaxError("Error in 'markdown' filter: The Python markdown library isn't installed.")
         return force_unicode(value)
     else:
-        markdown_vers = getattr(markdown, "version_info", 0)
-        if markdown_vers < (2, 1):
-            if settings.DEBUG:
-                raise template.TemplateSyntaxError(
-                    "Error in 'markdown' filter: Django does not support versions of the Python markdown library < 2.1.")
-            return force_unicode(value)
-        else:
-            return mark_safe(markdown.markdown(
-                force_unicode(value), ['smartypants','onion'], safe_mode=False))
+        return mark_safe(markdown.markdown(force_unicode(value), ['smartypants','onion'], safe_mode=False))
