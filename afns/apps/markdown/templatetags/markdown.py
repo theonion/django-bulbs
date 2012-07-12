81fa38e946b811bcbe53519e44a8213ce33e3c93
Chris Sinchok <csinchok@theonion.com>
2012-07-12 11:27:19 -0500

    I trust that I'll have the right markdown lib installed

--- afns/apps/markdown/templatetags/markdown.py
+++ afns/apps/markdown/templatetags/markdown.py
@@ -8,10 +8,10 @@ register = template.Library()
 @register.filter(is_safe=True)
 def markdown(value, arg=''):
     try:
-        import markdown
+        import markdown as markdown_lib
     except ImportError:
         if settings.DEBUG:
             raise template.TemplateSyntaxError("Error in 'markdown' filter: The Python markdown library isn't installed.")
         return force_unicode(value)
     else:
-        return mark_safe(markdown.markdown(force_unicode(value), ['smartypants','onion']), safe_mode=False)
+        return mark_safe(markdown_lib.markdown(force_unicode(value), ['smartypants','onion']), safe_mode=False)
