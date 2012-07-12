505b9dd706b0ff3bacf081baecd4711993f121ee
Chris Sinchok <csinchok@theonion.com>
2012-07-12 11:28:38 -0500

    Oh man, these package names are no good

--- afns/apps/markdown/templatetags/markdown.py
+++ afns/apps/markdown/templatetags/markdown.py
@@ -1,3 +1,5 @@
+import markdown
+
 from django import template
 from django.conf import settings
 from django.utils.encoding import smart_str, force_unicode
@@ -7,11 +9,4 @@ register = template.Library()
 
 @register.filter(is_safe=True)
 def markdown(value, arg=''):
-    try:
-        import markdown as markdown_lib
-    except ImportError:
-        if settings.DEBUG:
-            raise template.TemplateSyntaxError("Error in 'markdown' filter: The Python markdown library isn't installed.")
-        return force_unicode(value)
-    else:
-        return mark_safe(markdown_lib.markdown(force_unicode(value), ['smartypants','onion']), safe_mode=False)
+    return mark_safe(markdown.markdown(force_unicode(value), ['smartypants','onion']), safe_mode=False)
