ff2fe236df013aaf19b8b069d5877e7b060eb61a
Chris Sinchok <csinchok@theonion.com>
2012-07-12 10:29:55 -0500

    Trying a new setup

--- setup.py
+++ setup.py
@@ -1,14 +1,73 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
+import re
+import os
+import sys
 
-setup(name="django-afns",
-      version='1.0',
-      description="America's finest namespace",
-      author='Chris Sinchok',
-      author_email='csinchok@theonion.com',
-      url='http://gitlab.theonion.com/afns',
-      packages = find_packages(),
-      include_package_data = True,
-      zip_safe = False
-     )
+
+name = 'django-afns'
+package = 'afns'
+description = ''
+url = 'http://gitlab.theonion.com/afns'
+author = 'Chris Sinchok'
+author_email = 'chris@sinchok.com'
+license = 'BSD'
+install_requires = []
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    args = {'version': get_version(package)}
+    print "You probably want to also tag the version now:"
+    print "  git tag -a %(version)s -m 'version %(version)s'" % args
+    print "  git push --tags"
+    sys.exit()
+
+
+setup(
+    name=name,
+    version=get_version(package),
+    url=url,
+    license=license,
+    description=description,
+    author=author,
+    author_email=author_email,
+    packages=get_packages(package),
+    package_data=get_package_data(package),
+    install_requires=install_requires
+)
