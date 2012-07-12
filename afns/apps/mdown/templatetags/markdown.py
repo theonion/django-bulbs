1238cc324a2e504bb9b46e51711b285ac01ceeb8
Chris Sinchok <csinchok@theonion.com>
2012-07-12 11:43:33 -0500

    mdown rename

---
+++ afns/apps/mdown/templatetags/markdown.py
@@ -0,0 +1,16 @@
+from django import template
+from django.conf import settings
+from django.utils.encoding import smart_str, force_unicode
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+def markdown(value, arg=''):
+    try:
+        import markdown
+    except ImportError:
+        if settings.DEBUG:
+            raise template.TemplateSyntaxError("Error in 'markdown' filter: The Python markdown library isn't installed.")
+        return force_unicode(value)
+    return mark_safe(markdown_lib.markdown(force_unicode(value), ['smartypants','onion']), safe_mode=False)
