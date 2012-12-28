f751766fc6486b626adf876e3e9fd23bd51998d9
Chris Sinchok <csinchok@theonion.com>
2012-12-28 14:46:35 -0600

    Adding youtube tech

--- bulbs/videos/admin.py
+++ bulbs/videos/admin.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib import admin
 
-from bulbs.base.admin import ContentInline
 from bulbs.videos.models import Video, VideoSource
 from bulbs.videos.widgets import AmazonUploadWidget
 
@@ -15,8 +14,5 @@ class VideoAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.URLField: {'widget': AmazonUploadWidget}
     }
-    inlines = [
-        ContentInline,
-    ]
 
 admin.site.register(Video, VideoAdmin)
