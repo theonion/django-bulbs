f7a63dd3145428f71ee587e923c8bc335a578927
Matt Bone <mbone@theonion.com>
2012-12-28 15:01:25 -0600

    only_type content manager, some admin changes

--- bulbs/base/models.py
+++ bulbs/base/models.py
@@ -13,13 +13,29 @@ class Tag(models.Model):
 class ContentManager(models.Manager):
     def tagged_as(self, *tag_names):
         """
+        Return content that's been tagged with tags of the specified names.
 
+        For example:
+
+            Content.objects.tagged_as("tag1", "tag2")
+
+        Will return objects tagged with tags named 'tag1' OR 'tag2.'
         """
         if tag_names:
             return super(ContentManager, self).get_query_set().filter(tags__name__in=tag_names).distinct()
         else:
             return super(ContentManager, self).get_query_set()
 
+
+    def only_type(self, cls_or_instance):
+        """
+        Only return content of the specified type.
+
+        For example:
+
+            Content.objects.only_type(TestContentObj).filter(author="mbone")
+        """
+        return super(ContentManager, self).filter(content_type=ContentType.objects.get_for_model(cls_or_instance))
 
 
 class Content(models.Model):
@@ -45,7 +61,7 @@ class Content(models.Model):
 
 
     class Meta:
-        unique_together = (('content_type', 'object_id'),)
+        unique_together = (('content_type', 'object_id'),)  # sets up a one-one-relationship between this and a child content object
         verbose_name_plural = "content"
 
 
@@ -58,6 +74,16 @@ class ContentMixin(object):
     def create_content(cls, **kwargs):
         """
         Create this object and the parent content object.
+
+        Pass all the regular kwargs you'd like to this method and pass kwargs to the parent Content object by prefixing
+        them with `content__`. For example:
+
+            TestContentObj.create_content(field1="my field one",
+                                          field2="my field two",
+                                          content__title="my title")
+
+        Creates a `TestContentObj` instance first with the fields you'd expect, AND a `Content` instance tied to the
+        `TestContentObj` instance that's just been created.
         """
         content_kwargs = {}
         obj_kwargs = {}
@@ -83,7 +109,10 @@ class ContentMixin(object):
     def content(self):
         """
         Return the corresponding `base.models.Content` object.
+
+        It should always exist, but if it doesn't, a `Content.DoesNotExist` exception will be raised.
         """
+        # TODO cache this.
         return Content.objects.get(object_id=self.pk,
                                    content_type=ContentType.objects.get_for_model(self).id)
 
