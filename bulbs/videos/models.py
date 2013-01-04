e3dc9137eddf885d422814db1d1464ae8f51237e
Chris Sinchok <csinchok@theonion.com>
2013-01-04 17:30:30 -0600

    Zencoder work

--- bulbs/videos/models.py
+++ bulbs/videos/models.py
@@ -1,5 +1,9 @@
-from django.db import models
+import requests
+import json
+import urlparse
 
+from django.db import models
+from django.conf import settings
 
 from bulbs.images.models import Image
 
@@ -11,6 +15,34 @@ class Video(models.Model):
 
     def __unicode__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        super(Video, self).save(*args, **kwargs)
+
+        if self.original and not self.sources.exists():
+            path = urlparse.urlparse(self.original).path
+            output_format_params = {
+                'bucket': settings.VIDEO_BUCKET,
+                'directory': settings.VIDEO_DIRECTORY,
+                'pk': self.pk,
+                'size': 'high'
+            }
+            payload = {
+                'input': 's3:/%s' % path,
+                'outputs': [
+                    {
+                        "format": "mp4",
+                        "public": True,
+                        "url": "s3://%(bucket)s/%(directory)s/%(pk)s/%(size)s.mp4" % output_format_params
+                    },
+                    {
+                        "format": "webm",
+                        "public": True,
+                        "url": "s3://%(bucket)s/%(directory)s/%(pk)s/%(size)s.webm" % output_format_params
+                    }
+                ]
+            }
+            requests.post('https://app.zencoder.com/api/v2/jobs', data=json.dumps(payload), headers={'Zencoder-Api-Key': settings.ZENCODER_API_KEY})
 
 
 class VideoSource(models.Model):
