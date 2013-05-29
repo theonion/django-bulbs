03a9f2b508706afc1d66fa47e7c261b74ecb9d1d
Chris Sinchok <csinchok@theonion.com>
2013-05-28 21:13:27 -0500

    Bugfix

--- bulbs/base/views.py
+++ bulbs/base/views.py
@@ -41,36 +41,6 @@ class ContentListView(ListView):
 
     def get_queryset(self):
         tags = self.tags or self.kwargs.get('tags') or self.request.GET.getlist('tag', [])
-        types = self.types or self.kwargs.get('types') or request.GET.get('type', [])
+        types = self.types or self.kwargs.get('types') or self.request.GET.get('type', [])
         published = self.published or self.kwargs.get('published') or self.request.GET.get('published', [])
         return Contentish.search(tags=tags, types=types, published=published)
-
-    # def get(self, request, *args, **kwargs):
-    #     tags = self.tags or kwargs.get('tags') or request.GET.get('tags')
-    #     content_type = self.content_type or kwargs.get('content_type') or request.GET.get('content_type')
-    #     published = self.published or kwargs.get('published') or request.GET.get('published')
-
-    #     queryset = Content.objects.search(tags=tags, content_type=content_type, published=published)
-    #     paginator = self.paginator_class(queryset, 25)
-
-    #     page = self.kwargs.get('page') or self.request.GET.get('page') or 1
-    #     try:
-    #         page_number = int(page)
-    #     except ValueError:
-    #         if page == 'last':
-    #             page_number = paginator.num_pages
-    #         else:
-    #             raise Http404(u"Page is not 'last', nor can it be converted to an int.")
-    #     try:
-    #         page = paginator.page(page_number)
-    #     except InvalidPage:
-    #         raise Http404(u'Invalid page (%s' % page_number)
-
-    #     context = {
-    #         'paginator': paginator,
-    #         'page_obj': page,
-    #         'is_paginated': page.has_other_pages(),
-    #         'object_list': page.object_list
-    #     }
-
-    #     return None
