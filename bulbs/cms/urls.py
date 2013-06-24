46263684226f133c116de3b9483f4a3790e9f74a
Chris Sinchok <csinchok@theonion.com>
2013-06-24 14:40:45 -0500

    Moving the cms app

--- bulbs/cms/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, patterns
-
-from bulbs.cms.views import AdminListView
-
-
-urlpatterns = patterns(
-    'bulbs.cms.views',
-    url(r'^list/', AdminListView.as_view())
-)
