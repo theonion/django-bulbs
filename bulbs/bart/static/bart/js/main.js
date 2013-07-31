ddce1f663e8412d30dedb664984138ef604a3883
Shawn Cook <scook@theonion.com>
2013-07-31 11:49:14 -0500

    some nav stuff for now

--- bulbs/bart/static/bart/js/main.js
+++ bulbs/bart/static/bart/js/main.js
@@ -1,5 +1,22 @@
 $( setup );
 
 function setup(){
-	
+	headerScroll()
+}
+
+function headerScroll(){
+	var header = 	$('header#primary').parent(),
+		top = 		$(window).scrollTop(),
+		topClass = 	'scrolled-up',
+		scrollPt =	15
+
+	$(window).load(function(){
+		if (top <= scrollPt){ header.addClass(topClass) } 
+	})	
+
+	$(window).scroll(function() {
+		var top = $(window).scrollTop()
+		if (top <= scrollPt){ header.addClass(topClass) } 
+		else { header.removeClass(topClass) }
+	})
 }
