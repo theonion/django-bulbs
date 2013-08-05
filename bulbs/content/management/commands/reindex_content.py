97b623181a906a1c224bd4344f694e5d2fc1967d
Adam Wentz <awentz@theonion.com>
2013-08-05 14:38:26 -0500

    Fixed `reindex_content` management command

--- bulbs/content/management/commands/reindex_content.py
+++ bulbs/content/management/commands/reindex_content.py
@@ -1,18 +1,20 @@
 from django.core.management.base import NoArgsCommand
-from bulbs.content.models import Content
+from bulbs.content.models import Content, Tag
 
 
 class Command(NoArgsCommand):
-    help = 'Runs Content.index on all content.'
+    help = 'Runs index on all Content and Tag instances.'
 
     def handle(self, **options):
-        num_processed = 0
-        content_count = Content.objects.all().count()
-        chunk_size = 10
-        while num_processed < content_count:
-            for content in Content.objects.all().order_by('id')[num_processed:num_processed + chunk_size]:
-                content.index()
-                num_processed += 1
-                if not num_processed % 100:
-                    print 'Processed %d content items' % num_processed
-
+        chunk_size = 100
+        for klass in (Content, Tag):
+            num_processed = 0
+            instance_count = klass.objects.all().count()       
+            while num_processed < instance_count:
+                for instance in klass.objects.all().order_by('id')[num_processed:num_processed + chunk_size]:
+                    instance.index()
+                    num_processed += 1
+                    if not num_processed % 100:
+                        print 'Processed %d %s items' % (
+                            num_processed, klass.__name__
+                        )
