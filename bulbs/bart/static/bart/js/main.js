103731d6e661f6c7bb3a05ddc6c2454c3d06b1ec
Shawn Cook <scook@theonion.com>
2013-08-06 11:52:12 -0500

    added ad banner and scroll behavior

--- bulbs/bart/static/bart/js/main.js
+++ bulbs/bart/static/bart/js/main.js
@@ -6,17 +6,28 @@ function setup(){
 
 function headerScroll(){
 	var header = 	$('header#primary').parent(), // .header-container
-		postBody =	$('.article-text p').first().offset().top - 80, // 80 above start of article body
-		top = 		$(window).scrollTop(), // Top of window (duh)
+		postBody =	$('.article-body p').first().offset().top - 80, // 80 above start of article body
+		top = 		$(window).scrollTop() + 90, // Top of window plus 90 for the banner
 		topClass = 	'scrolled-up' // Class to add/remove
 
 	$(window).load(function(){
 		if (top <= postBody){ header.addClass(topClass) } 
+		positionTheHeader()
 	})	
 
-	$(window).scroll(function() {
+	$(window).scroll(function(e){ 
 		var top = $(window).scrollTop()
 		if (top <= postBody){ header.addClass(topClass) } 
 		else { header.removeClass(topClass) }
-	})
+		positionTheHeader()
+	});
+
+	function positionTheHeader() {
+		$el = $('header#primary').parent(); 
+		if ($(this).scrollTop() > 90 && $el.css('position') != 'fixed'){ 
+			$el.css({'position': 'fixed', 'top': '0px'})
+		} else if ($(this).scrollTop() < 90 && $el.css('position') == 'fixed'){
+			$el.css({'position': 'absolute', 'top': '90px'})
+		}
+	}
 }
