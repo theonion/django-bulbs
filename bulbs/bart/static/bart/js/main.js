59b321b0a585e5767161cc4a96a8f29ddddeaf7e
Shawn Cook <scook@theonion.com>
2013-08-06 12:34:25 -0500

    me being smart

--- bulbs/bart/static/bart/js/main.js
+++ bulbs/bart/static/bart/js/main.js
@@ -1,33 +1,30 @@
 $( setup );
 
 function setup(){
-	headerScroll()
 }
 
 function headerScroll(){
-	var header = 	$('header#primary').parent(), // .header-container
+	var headerEl = 	$('header#primary').parent(), // .header-container
+		adHeight = 	90,
 		postBody =	$('.article-body p').first().offset().top - 80, // 80 above start of article body
-		top = 		$(window).scrollTop() + 90, // Top of window plus 90 for the banner
-		topClass = 	'scrolled-up' // Class to add/remove
+		topClass = 	'scrolled-up', // Class to add/remove
+		top = 		$(window).scrollTop(),
+		topWithAd = top + adHeight
 
-	$(window).load(function(){
-		if (top <= postBody){ header.addClass(topClass) } 
-		positionTheHeader()
-	})	
+	if (topWithAd <= postBody){ headerEl.addClass(topClass) } 
+		else { headerEl.removeClass(topClass) }
 
-	$(window).scroll(function(e){ 
-		var top = $(window).scrollTop()
-		if (top <= postBody){ header.addClass(topClass) } 
-		else { header.removeClass(topClass) }
-		positionTheHeader()
-	});
-
-	function positionTheHeader() {
-		$el = $('header#primary').parent(); 
-		if ($(this).scrollTop() > 90 && $el.css('position') != 'fixed'){ 
-			$el.css({'position': 'fixed', 'top': '0px'})
-		} else if ($(this).scrollTop() < 90 && $el.css('position') == 'fixed'){
-			$el.css({'position': 'absolute', 'top': '90px'})
-		}
+	if (top > adHeight){ 
+		headerEl.css({'position': 'fixed', 'top': '0px'})
+	} else {
+		headerEl.css({'position': 'absolute', 'top': adHeight + 'px'})
 	}
 }
+
+$(document).ready(function(){
+	headerScroll()
+})	
+
+$(window).scroll(function(e){ 
+	headerScroll()
+})
