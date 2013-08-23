dd298f99a682988274e824ce3268407d739afc0d
Adam Wentz <awentz@theonion.com>
2013-08-23 14:28:50 -0500

    Updated Content serializer to exclude polymorphic ctype field and added stub serializers for Image and User so the browesable API for Content doesn't blow up rendering the form.

--- bulbs/content/serializers.py
+++ bulbs/content/serializers.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
+from bulbs.images.models import Image
+
 from .models import Content, Tag
 
 
@@ -8,24 +12,39 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
 
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+
+
 class ContentSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name='content-detail',
         lookup_field='pk'
     )
     tags = TagSerializer(many=True)
+    authors = AuthorSerializer(many=True)
+    image = ImageSerializer()
 
     class Meta:
         model = Content
+        exclude = ('polymorphic_ctype',)
 
 
 class PolymorphicContentSerializer(ContentSerializer):
     def to_native(self, value):
         # TODO: allow per-class definitions of serializers like this:
         # real_serializer = value.get_serializer()
+        
         # For now, here's the simplest possible thing:
         class ThisSerializer(serializers.ModelSerializer):
             class Meta:
                 model = value.__class__
+        
         real_serializer = ThisSerializer()
         return real_serializer.to_native(value)
