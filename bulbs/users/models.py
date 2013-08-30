90df742e1baedef77dcf040626c64ab07da9a738
Chris Sinchok <csinchok@theonion.com>
2013-08-30 13:45:55 -0500

    Killings users

--- bulbs/users/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractBaseUser
-from django.db import models
-
-
-class User(AbstractBaseUser):
-    username = models.CharField(max_length=40, unique=True, db_index=True)
-    USERNAME_FIELD = 'username'
