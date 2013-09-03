afa7d60336cd2eec802ebb4cf707100665393feb
Chris Sinchok <csinchok@theonion.com>
2013-09-03 16:45:23 -0500

    Ugh, ignore

---
+++ bulbs/images/fields.py
@@ -0,0 +1,25 @@
+from django.utils.translation import ugettext_lazy as _
+from django.db.models.fields.files import FileField, FieldFile
+from bulbs.images.conf import settings
+
+from bulbs.images.storages import BettyCropperStorage
+
+
+class RemoteImageFieldFile(FieldFile):
+    def crop_url(self, width, ratio="original", format="jpg"):
+        return "%s/%s/%s/%d.%s" % (settings.BETTY_CROPPER['PUBLIC_URL'], self.file.name, ratio, width, format)
+
+class RemoteImageField(FileField):
+    description = _("RemoteImage")
+    attr_class = RemoteImageFieldFile
+
+    def __init__(self, verbose_name=None, name=None, upload_to='', storage=None, **kwargs):
+        for arg in ('primary_key', 'unique'):
+            if arg in kwargs:
+                raise TypeError("'%s' is not a valid argument for %s." % (arg, self.__class__))
+
+        self.storage = BettyCropperStorage()
+        self.upload_to = settings.BETTY_CROPPER['ADMIN_URL']
+
+        kwargs['max_length'] = kwargs.get('max_length', 100)
+        super(FileField, self).__init__(verbose_name, name, **kwargs)
---
+++ bulbs/images/storages.py
@@ -0,0 +1,36 @@
+import os
+
+from django.core.files.storage import Storage
+from bulbs.images.conf import settings
+
+import requests
+
+
+class BettyCropperStorage(Storage):
+
+    def delete(self, name):
+    	raise NotImplementedError
+
+    def exists(self, name):
+    	src_url = "%s/%s/src" % (settings.BETTY_CROPPER['PUBLIC_URL'], name)
+    	r = requests.head(src_url)
+    	return r.status_code == 200
+
+    def listdir(self, path):
+    	raise NotImplementedError
+
+    def save(self, name, content):
+    	new_url = "%s/api/new" % settings.BETTY_CROPPER['ADMIN_URL']
+    	files = {
+    		'image': (os.path.basename(content.name), content)
+    	}
+    	r = requests.post(new_url, files=files)
+    	if r.status_code != 201:
+    		raise Exception("Couldn't save file to URL: \"%s\" (code %s, message: %s)" % (new_url, r.status_code, r.content))
+    	return str(r.json()['id'])
+
+    def size(self, name):
+    	raise NotImplementedError
+
+    def url(self, name):
+    	return "%s/%s/original/600.jpg" % (settings.BETTY_CROPPER['PUBLIC_URL'], name)
