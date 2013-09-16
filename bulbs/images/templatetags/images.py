90149a1937b37f50b574c5e6b9f14f3f4311c774
Chris Sinchok <csinchok@theonion.com>
2013-09-16 18:13:05 -0500

    New image defaults

--- bulbs/images/templatetags/images.py
+++ bulbs/images/templatetags/images.py
@@ -8,6 +8,7 @@ register = template.Library()
 def crop_url(image, width, ratio="original", format="jpg"):
     return "%s/%s/%s/%s.%s" % (settings.BETTY_CROPPER['PUBLIC_URL'], image, ratio, width, format)
 
+
 @register.simple_tag
 def image_url(image, width, ratio='16x9', format='jpg'):
     if width is None:
@@ -16,6 +17,7 @@ def image_url(image, width, ratio='16x9', format='jpg'):
         return image.crop_url(width=width, ratio=ratio, format=format)
     else:
         return crop_url(image, width=width, ratio=ratio, format=format)
+
 
 @register.inclusion_tag('images/image.html', takes_context=True)
 def image(context, image, width, ratio='16x9', format="jpg"):
