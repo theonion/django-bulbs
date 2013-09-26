c2ceb871474cf5869feb412139802718c35224b0
Adam Wentz <awentz@theonion.com>
2013-09-26 10:37:01 -0500

    Updated bulbs serializers to use custom auth model

--- bulbs/content/serializers.py
+++ bulbs/content/serializers.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib import auth
 from rest_framework import serializers
 
 from .models import Content, Tag
@@ -12,13 +12,13 @@ class TagSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = auth.get_user_model()
         exclude = ('password',)
 
 
 class SimpleAuthorSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = auth.get_user_model()
         fields = ('id', 'first_name', 'last_name')
 
 
