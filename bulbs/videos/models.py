822cfd41d0c7737b85dfd1ea5cc8b9665bff629c
Chris Sinchok <csinchok@theonion.com>
2013-10-09 17:13:19 -0500

    Image Field

--- bulbs/videos/models.py
+++ bulbs/videos/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from json_field import JSONField
+from bulbs.images.fields import RemoteImageField
 
 
 DEFAULT_VIDEO_OUTPUT = {
@@ -28,6 +29,7 @@ class Video(models.Model):
     name = models.CharField(max_length=255)
     data = JSONField(null=True, blank=True, help_text="This is JSON taht is returned from an encoding job")
     sources = JSONField(null=True, blank=True, default=[], help_text="This is a JSON array of sources.")
+    poster = RemoteImageField(null=True, blank=True)
 
     def __unicode__(self):
         return self.name
