ae0ec5cf6670e4d17d513ccbac40c64af4de7bde
Adam Wentz <awentz@theonion.com>
2014-01-13 15:38:14 -0600

    Typo

--- bulbs/content/__init__.py
+++ bulbs/content/__init__.py
@@ -5,7 +5,7 @@ class TagCache:
     @classmethod
     def count(cls, slug):
         from .models import Tag
-        # Gets the count for a tag, hopefully form an in-memory cache.
+        # Gets the count for a tag, hopefully from an in-memory cache.
         cnt = cls._cache.get(slug)
         if cnt is None:
             cnt = Tag.search_objects.s().query(**{"tags.slug": slug}).count()
