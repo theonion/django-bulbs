844783a4df74b1b05b41bb9536df4de321343519
Chris Sinchok <csinchok@theonion.com>
2014-01-22 10:47:42 -0600

    Fixing video URLs

--- bulbs/videos/urls.py
+++ bulbs/videos/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
+
+from bulbs.videos.api import router
 
 urlpatterns = patterns('bulbs.videos.views',
+    url(r'api/', include(router.urls)),
     url(r'videoAttrs\.js$', 'video_attrs'),
     url(r'notification$', 'notification')
 )
