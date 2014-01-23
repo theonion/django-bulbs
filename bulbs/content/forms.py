993cc74ba9be4f54a813e1513cde832b923b0cf3
Adam Wentz <awentz@theonion.com>
2014-01-23 12:38:07 -0600

    Replaced DoctypeChoiceField with a lazily evaluated function

--- bulbs/content/forms.py
+++ bulbs/content/forms.py
@@ -1,19 +1,23 @@
 from django import forms
+from django.utils.functional import lazy
 
 from .models import Content
 
 
-class DoctypeChoiceField(forms.ChoiceField):
-    """Field for choosing amongst the doctypes of a given polymorphic model."""
-    def __init__(self, model, exclude_base=False, *args, **kwargs):
+def mapping_type_choices(model, exclude_base=True):
+    def get_choices():
         mapping_type_models = model.get_mapping_type_models(exclude_base=exclude_base)
         choices = []
         for mapping_type_name, content_type in mapping_type_models:
             choices.append((mapping_type_name, content_type.name))
-        super(DoctypeChoiceField, self).__init__(choices=choices, *args, **kwargs)
+        return choices
+    return get_choices
 
 
 class ContentDoctypeForm(forms.Form):
     """Form for choosing a Content subclass doctype."""
-    doctype = DoctypeChoiceField(Content, exclude_base=True, label='Type')
+    doctype = forms.ChoiceField(
+        choices=lazy(mapping_type_choices(Content, exclude_base=True), list)(),
+        label='Type'
+    )
 
