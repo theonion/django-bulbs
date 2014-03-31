177d7c77088907c8010f19cfd8774da7542c62b3
Chris Sinchok <csinchok@theonion.com>
2014-03-31 16:24:03 -0500

    Adding pytest

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import os
+import tempfile
+
+from django.conf import settings
+
+
+MODULE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+
+def pytest_configure():
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:'
+            }
+        },
+        MEDIA_ROOT=tempfile.mkdtemp("bettycropper"),
+        TEMPLATE_DIRS=(os.path.join(MODULE_ROOT, 'tests', 'templates'),),
+        INSTALLED_APPS=(
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "rest_framework",
+            "polymorphic",
+            "elastimorphic",
+            "bulbs.content",
+            "bulbs.images",
+            "tests.testcontent",),
+        ROOT_URLCONF = 'tests.urls',
+        
+        ES_DISABLED = False,
+        ES_URLS = ['http://localhost:9200'],
+    )
