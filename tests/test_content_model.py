e6ca28c29e538c37255ad3f98bda0213be786e79
Chris Sinchok <csinchok@theonion.com>
2014-04-03 14:44:47 -0500

    Killing unused(?) S classes

--- tests/test_content_model.py
+++ tests/test_content_model.py
@@ -21,3 +21,15 @@ class SerializerTestCase(BaseIndexableTestCase):
 
         content.published = timezone.now() + datetime.timedelta(hours=1)
         self.assertEqual(content.get_status(), "final")
+
+    def test_is_published(self):
+        content = TestContentObj.objects.create(
+            title="Unpublished article"
+        )
+        self.assertFalse(content.is_published)
+
+        content.published = timezone.now() - datetime.timedelta(hours=1)
+        self.assertTrue(content.is_published)
+
+        content.published = timezone.now() + datetime.timedelta(hours=1)
+        self.assertFalse(content.is_published)
