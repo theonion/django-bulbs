55304acb3932f217eb742399427c58c56ee9fd16
Chris Sinchok <csinchok@theonion.com>
2014-04-09 10:11:07 -0500

    Adding some filez

---
+++ bulbs/promotion/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import ContentList
+
+
+class ContentListField(serializers.WritableField):
+    def to_native(self, obj):
+        content = []
+        for c in obj:
+            serializer = c.get_serializer_class()
+            content.append(serializer(instance=c).data)
+        return content
+
+    def from_native(self, data):
+        return [content_data["id"] for content_data in data]
+
+
+class ContentListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContentList
+        exclude = ("content_ids",)
+
+    content = ContentListField(source="content")
---
+++ tests/test_contentlist.py
@@ -0,0 +1,62 @@
+import json
+
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+from django.test.client import Client
+
+from elastimorphic.tests.base import BaseIndexableTestCase
+
+from bulbs.promotion.models import ContentList
+
+from tests.testcontent.models import TestContentObj
+
+
+class ContentListTestCase(BaseIndexableTestCase):
+
+    def test_content_list(self):
+        content_list = ContentList.objects.create(name="homepage")
+        content_ids = []
+        for i in range(10):
+            content = TestContentObj.objects.create(
+                title="Content test #{}".format(i),
+            )
+            content_ids.append(content.pk)
+
+        content_list.content = content_ids
+        content_list.save()
+
+        self.assertEqual(len(content_list.content), 10)
+        for index, content in enumerate(content_list.content):
+            self.assertEqual(content.title, "Content test #{}".format(index))
+
+    def test_content_list_api(self):
+        User = get_user_model()
+        admin = User.objects.create_user("admin", "tech@theonion.com", "secret")
+        admin.is_staff = True
+        admin.save()
+        client = Client()
+        client.login(username="admin", password="secret")
+
+        content_list = ContentList.objects.create(name="homepage")
+        content_ids = []
+        for i in range(10):
+            content = TestContentObj.objects.create(
+                title="Content test #{}".format(i),
+            )
+            content_ids.append(content.pk)
+        content_list.content = content_ids
+        content_list.save()
+        endpoint = reverse("contentlist-detail", kwargs={"pk": content_list.pk})
+
+        response = client.get(endpoint)
+        self.assertEqual(response.status_code, 200)
+        for index, content in enumerate(response.data["content"]):
+            self.assertEqual(content["title"], "Content test #{}".format(index))
+
+        new_data = response.data
+        new_data["content"] = [{"id": content["id"]} for content in response.data["content"]][::-1]
+
+        response = client.put(endpoint, json.dumps(new_data), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        for index, content in enumerate(response.data["content"]):
+            self.assertEqual(content["title"], "Content test #{}".format(9 - index))
