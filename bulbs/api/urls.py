de337b8f984fd55208de6987be7b2aa9d70ffe03
Chris Sinchok <csinchok@theonion.com>
2014-09-11 11:50:10 -0500

    Fixing the logout urls, etc

--- bulbs/api/urls.py
+++ bulbs/api/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from .views import api_v1_router, MeViewSet
 
 urlpatterns = (
-    url(r"^me/logout$", "django.contrib.auth.views.logout", name="logout"),
-    url(r'^me', MeViewSet.as_view({'get': 'retrieve'}), name='me'),
+    url(r"^me/logout/?$", "django.contrib.auth.views.logout", name="logout"),
+    url(r'^me/?$', MeViewSet.as_view({'get': 'retrieve'}), name='me'),
     url(r"^", include(api_v1_router.urls))       # noqa
 )
