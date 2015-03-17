f484ef6465ad6df762bebc107a4c5c2c14e521e5
Chris Sinchok <csinchok@theonion.com>
2015-03-17 14:46:10 -0500

    let's move the import inside the tests, to avoid some errors

--- tests/campaigns/test_campaign_serializer.py
+++ tests/campaigns/test_campaign_serializer.py
@@ -1,18 +1,21 @@
 from django.test import TestCase
 
 from bulbs.campaigns.models import CampaignPixel
-from bulbs.campaigns.serializers import CampaignTypeField
 
 
 class CampaignTypeFieldCase(TestCase):
 
     def test_from_native(self):
+        from bulbs.campaigns.serializers import CampaignTypeField
+
         field = CampaignTypeField()
         self.assertEqual(CampaignPixel.LOGO, field.from_native('Logo'))
         with self.assertRaises(KeyError):
             field.from_native(None)
 
     def test_to_native(self):
+        from bulbs.campaigns.serializers import CampaignTypeField
+
         field = CampaignTypeField()
         self.assertEqual('Logo', field.to_native(CampaignPixel.LOGO))
         with self.assertRaises(KeyError):
