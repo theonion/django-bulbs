a428d3d6e734e42400b3d01078221b62e6b77d84
Mike Parent <mparent@umich.edu>
2015-03-17 10:47:17 -0500

    Added new campaign model

---
+++ tests/campaigns/test_campaign_model.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+from bulbs.campaigns.models import Campaign, CampaignPixel
+
+
+class CampaignModelCase(TestCase):
+
+
+    def test_model(self):
+        campaign = Campaign.objects.create(sponsor_name='Sponsor',
+                                           campaign_label='Label')
+        self.assertIsNotNone(campaign.id)
