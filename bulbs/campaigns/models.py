6379227a972e682f2ddc361e4f8e4b5c5175c09e
Chris Sinchok <csinchok@theonion.com>
2015-03-17 10:52:09 -0500

    Changing sponsor_logo to a djbetty image, adding initial migrations

--- bulbs/campaigns/models.py
+++ bulbs/campaigns/models.py
@@ -6,7 +6,7 @@ from djbetty import ImageField
 class Campaign(models.Model):
 
     sponsor_name = models.CharField(max_length=255)
-    sponsor_logo = models.ImageField(null=True, blank=True)
+    sponsor_logo = ImageField(null=True, blank=True)
     sponsor_url = models.URLField(null=True, blank=True)
     start_date = models.DateTimeField(null=True, blank=True)
     end_date = models.DateTimeField(null=True, blank=True)
