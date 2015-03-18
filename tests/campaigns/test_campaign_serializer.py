b100a74d71b6deda284aa659940f716f5745c6bc
Mike Parent <mparent@umich.edu>
2015-03-18 15:32:43 -0500

    Campaigns: serializer test local-import workaround for Travis test failure

--- tests/campaigns/test_campaign_serializer.py
+++ tests/campaigns/test_campaign_serializer.py
@@ -1,18 +1,21 @@
 from django.test import TestCase
 
 from bulbs.campaigns.models import CampaignPixel
-from bulbs.campaigns.serializers import PixelTypeField
 
 
 class PixelTypeFieldCase(TestCase):
 
     def test_from_native(self):
+        # TODO: top-level import causes Travis test to fail
+        from bulbs.campaigns.serializers import PixelTypeField
         field = PixelTypeField()
         self.assertEqual(CampaignPixel.LOGO, field.from_native('Logo'))
         with self.assertRaises(KeyError):
             field.from_native(None)
 
     def test_to_native(self):
+        # TODO: top-level import causes Travis test to fail
+        from bulbs.campaigns.serializers import PixelTypeField
         field = PixelTypeField()
         self.assertEqual('Logo', field.to_native(CampaignPixel.LOGO))
         with self.assertRaises(KeyError):
