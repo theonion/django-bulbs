d44aa184f8255b0a9dfc1930a61855aefe309fd4
vforgione <v.forgione@gmail.com>
2015-03-19 13:21:22 -0500

    added tests for special coverage querying

--- bulbs/special_coverage/models.py
+++ bulbs/special_coverage/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from json_field import JSONField
+
 from bulbs.campaigns.models import Campaign
+from bulbs.content.custom_search import custom_search_model
+from bulbs.content.models import Content
 
 
 class SpecialCoverage(models.Model):
@@ -17,3 +20,35 @@ class SpecialCoverage(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         return super(SpecialCoverage, self).save(*args, **kwargs)
+
+    def get_content(self):
+        """performs es search and gets content objects
+        """
+        if "query" in self.query:
+            q = self.query["query"]
+        else:
+            q = self.query
+        search = custom_search_model(Content, q, field_map={
+            "feature-type": "feature_type.slug",
+            "tag": "tags.slug",
+            "content-type": "_type"
+        })
+        return search.full()
+
+    @property
+    def contents(self):
+        if not hasattr(self, "_content"):
+            self._content = self.get_content()
+        return self._content
+
+    @property
+    def has_pinned_content(self):
+        """determines if the there is a pinned object in the search
+        """
+        if "query" in self.query:
+            q = self.query["query"]
+        else:
+            q = self.query
+        if "pinned_ids" in q:
+            return bool(len(q.get("pinned_ids", [])))
+        return False
