4b4e23c5215d4cd050396e66774b072553255a49
Mike Parent <mparent@umich.edu>
2015-03-23 12:34:33 -0500

    Campaigns: Fix REST handling of nested CampaignPixel objects. Will not support adding pixels until parent Campaign object has been creaetd (saved once), which will required UI support.

--- bulbs/campaigns/serializers.py
+++ bulbs/campaigns/serializers.py
@@ -1,5 +1,5 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
-
 from rest_framework import relations
 
 from bulbs.content.serializers import ImageFieldSerializer
@@ -23,25 +23,24 @@ class PixelTypeField(serializers.WritableField):
         return pixel_type_string_to_value(data)
 
 
-# class CampaignPixelSerializer(serializers.ModelSerializer):
 class CampaignPixelField(relations.RelatedField):
-
-    # pixel_type = PixelTypeField()
-
-    # class Meta:
-    #     model = CampaignPixel
-    #     exclude = ('campaign',)
 
     read_only = False
 
+    def to_native(self, obj):
+        return {'id': obj.id,
+                'pixel_type': dict(CampaignPixel.PIXEL_TYPES)[obj.pixel_type],
+                'url': obj.url}
+
     def from_native(self, data):
-        print(self.parent.object)
-        # raise Exception()
-        #import pytest; pytest.set_trace()
         if "id" in data:
             pixel = CampaignPixel.objects.get(id=data["id"])
         else:
-            #import pytest; pytest.set_trace()
+            # Django REST Framework has poor support for nested relations (for new parent objects,
+            # this from_native() method would be called before parent object is saved/available).
+            if self.parent.object is None:
+                raise ValidationError("New campaigns must be saved once before adding pixels.")
+
             url = data["url"]
             pixel_type = pixel_type_string_to_value(data["pixel_type"])
             pixel = CampaignPixel.objects.create(url=url,
