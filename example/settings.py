4c311444115f87f96c671122700a9f674baca35b
Chris Sinchok <csinchok@theonion.com>
2015-05-26 13:14:54 -0500

    Analyzer settings

--- example/settings.py
+++ example/settings.py
@@ -86,22 +86,24 @@ ES_INDEX = "django-bulbs"
 
 ES_INDEX_SETTINGS = {
     "django-bulbs": {
-        "analysis": {
-            "filter": {
-                "autocomplete_filter": {
-                    "type":     "edge_ngram",
-                    "min_gram": 1,
-                    "max_gram": 20
-                }
-            },
-            "analyzer": {
-                "autocomplete": {
-                    "type":      "custom",
-                    "tokenizer": "standard",
-                    "filter": [
-                        "lowercase",
-                        "autocomplete_filter" 
-                    ]
+        "index": {
+            "analysis": {
+                "filter": {
+                    "autocomplete_filter": {
+                        "type":     "edge_ngram",
+                        "min_gram": 1,
+                        "max_gram": 20
+                    }
+                },
+                "analyzer": {
+                    "autocomplete": {
+                        "type":      "custom",
+                        "tokenizer": "standard",
+                        "filter": [
+                            "lowercase",
+                            "autocomplete_filter" 
+                        ]
+                    }
                 }
             }
         }
