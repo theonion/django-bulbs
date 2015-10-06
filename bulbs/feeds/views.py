7ad923d3ef1e8e0d80674d9f57284b38dea1e0f2
Michael Butkovic <michael.butkovic@gmail.com>
2015-10-06 16:28:08 -0500

    change inheritance

--- bulbs/feeds/views.py
+++ bulbs/feeds/views.py
@@ -45,21 +45,9 @@ class RSSView(ContentListView):
         return RequestContext(self.request, context)
 
 
-class SpecialCoverageRSSView(ContentListView):
+class SpecialCoverageRSSView(RSSView):
     """Really simply, this syndicates Content."""
-    template_name = "feeds/rss.xml"
-    paginate_by = 20
     feed_title = "Special Coverage RSS Feed"
-    utm_params = "utm_medium=RSS&amp;utm_campaign=feeds"
-
-    def get_template_names(self):
-        return ["feeds/rss.xml", "feeds/_rss.xml"]
-
-    @cache_control(max_age=600)
-    def get(self, request, *args, **kwargs):
-        response = super(SpecialCoverageRSSView, self).get(request, *args, **kwargs)
-        response["Content-Type"] = "application/rss+xml"
-        return response
 
     def get_queryset(self):
         sc_id = self.request.GET.get("special_coverage_id")
@@ -72,21 +60,4 @@ class SpecialCoverageRSSView(ContentListView):
         else:
             sc_ids = []
 
-        return super(SpecialCoverageRSSView, self).get_queryset().filter(Ids(values=sc_ids))[:self.paginate_by]
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(SpecialCoverageRSSView, self).get_context_data(*args, **kwargs)
-        context["full"] = (self.request.GET.get("full", "false").lower() == "true")
-        context["images"] = (self.request.GET.get("images", "false").lower() == "true")
-        context["build_date"] = now()
-        context["title"] = self.feed_title
-        context["feed_url"] = self.request.build_absolute_uri()
-        context["search_url"] = self.request.build_absolute_uri(
-            u"/search?%s" % self.request.META["QUERY_STRING"])
-
-        # OK, so this is kinda brutal. Stay with me here.
-        for content in context["page_obj"].object_list:
-            feed_path = content.get_absolute_url() + "?" + self.utm_params
-            content.feed_url = self.request.build_absolute_uri(feed_path)
-
-        return RequestContext(self.request, context)
+        return ContentListView.get_queryset(self).filter(Ids(values=sc_ids))[:self.paginate_by]
