48fa27d48e116adaa4b667ed3caa020bc77252b9
Cameron Lowe <cam.m.lowe@gmail.com>
2015-11-06 13:03:21 -0600

    add start argument

--- bulbs/contributions/management/commands/backfill_contributions.py
+++ bulbs/contributions/management/commands/backfill_contributions.py
@@ -1,3 +1,5 @@
+import argparse
+
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
@@ -5,11 +7,35 @@ from bulbs.content.models import Content
 from bulbs.contributions.models import ContributorRole
 
 
+def valid_date(s):
+    try:
+        return timezone.datetime.strptime(s, "%Y-%m-%d")
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
+
 class Command(BaseCommand):
 
     help = "add missing contributions from authors of the previous month."
     default_role = ContributorRole.objects.first()
     count = 0
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-start',
+            '--start',
+            help='The Start Date - format YYYY-MM-DD',
+            required=False,
+            type=valid_date
+        )
+        parser.add_argument(
+            '-end',
+            '--end',
+            help='The End Date - format YYYY-MM-DD',
+            required=False,
+            type=valid_date
+        )
 
     def get_date_range(self):
         now = timezone.now()
@@ -34,10 +60,15 @@ class Command(BaseCommand):
                 self.count += 1
 
     def handle(self, *args, **kwargs):
+        import pdb; pdb.set_trace()
+
         if not self.default_role:
             self.stdout.write('No role available for backfill')
             return
-        start, end = self.get_date_range()
+
+        default_start, default_end = self.get_date_range()
+        start = kwargs.get('start', default_start)
+        end = kwargs.get('end', default_end)
         content_qs = self.get_content_list(start, end)
         for content in content_qs.all():
             self.process_content_contributions(content)
