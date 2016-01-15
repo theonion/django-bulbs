35390e84eb43c05df3e76cc4e6d68fd7cbfc61f7
Mike Parent <mparent@umich.edu>
2016-01-15 16:57:17 -0600

    Initial special coverage resolve.
    WIP. Needs to add filtering

--- bulbs/api/views.py
+++ bulbs/api/views.py
@@ -43,6 +43,8 @@ from bulbs.content.serializers import (
 
 from bulbs.contributions.serializers import ContributionSerializer
 from bulbs.contributions.models import Contribution
+from bulbs.special_coverage.models import SpecialCoverage
+from bulbs.special_coverage.serializers import SpecialCoverageSerializer
 
 from bulbs.utils.methods import get_query_params, get_request_data
 
@@ -466,9 +468,42 @@ class ContentResolveViewSet(viewsets.ReadOnlyModelViewSet):
                 raise Http404("No content found matching UUID")
 
             content = get_object_or_404(Content, pk=match.kwargs.get('pk'))
-            return Response(ContentSerializer().to_representation(content))
+            return Response(ContentSerializer(content).data)
         else:
             raise Http404('Must specify content "url" param')
+
+
+class ResolveSpecialCoverageViewSet(viewsets.ReadOnlyModelViewSet):
+    """Retrieve special coverage containing specified content"""
+    model = SpecialCoverage
+
+    def list(self, request):
+        content_id = get_query_params(self.request).get("content_id")
+        if content_id:
+            content = get_object_or_404(Content, pk=content_id)
+            # TODO: Filter active?
+            special_coverage_filter = {
+                "filter": {
+                    "prefix": {"_id": "specialcoverage"}
+                }
+            }
+            search_objects = type(content).search_objects
+            results = search_objects.client.percolate(
+                index=search_objects.mapping.index,
+                doc_type=search_objects.mapping.doc_type,
+                id=content.id,
+                body=special_coverage_filter,
+            )
+            if results["total"]:
+                special_coverage_ids = [int(m["_id"].split(".")[-1]) for m in results["matches"]]
+                qs = SpecialCoverage.objects.filter(id__in=special_coverage_ids).all()
+                serializer = SpecialCoverageSerializer(qs, many=True)
+                return Response(serializer.data)
+            else:
+                return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise Http404('Must specify content "url" param')
+
 
 
 class CustomSearchContentViewSet(viewsets.GenericViewSet):
@@ -543,6 +578,7 @@ api_v1_router.register(r"content", ContentViewSet, base_name="content")
 api_v1_router.register(r"custom-search-content", CustomSearchContentViewSet, base_name="custom-search-content")
 api_v1_router.register(r"content-type", ContentTypeViewSet, base_name="content-type")
 api_v1_router.register(r"content-resolve", ContentResolveViewSet, base_name="content-resolve")
+api_v1_router.register(r"resolve-special-coverage", ResolveSpecialCoverageViewSet, base_name="resolve-special-coverage")
 api_v1_router.register(r"tag", TagViewSet, base_name="tag")
 api_v1_router.register(r"log", LogEntryViewSet, base_name="logentry")
 api_v1_router.register(r"author", AuthorViewSet, base_name="author")
---
+++ tests/api/test_resolve_api.py
@@ -0,0 +1,105 @@
+from datetime import timedelta
+
+from django.core.urlresolvers import reverse
+from django.utils import timezone
+
+from bulbs.content.models import Tag, Content
+from bulbs.special_coverage.models import SpecialCoverage
+from bulbs.utils.test import BaseAPITestCase, make_content
+
+
+class TestResolveSpecialCoverageAPI(BaseAPITestCase):
+
+    def setUp(self):
+        super(TestResolveSpecialCoverageAPI, self).setUp()
+        biden_condition = {
+            "values": [{
+                "value": "joe-biden",
+                "label": "Joe Biden"
+            }],
+            "type": "all",
+            "field": "tag"
+        }
+        obama_condition = {
+            "values": [{
+                "value": "obama",
+                "label": "Obama"
+            }],
+            "type": "all",
+            "field": "tag"
+        }
+        biden_query = {
+            "label": "Uncle Joe",
+            "query": {
+                "groups": [{
+                    "conditions": [biden_condition]
+                }]
+            },
+        }
+        obama_query = {
+            "label": "Obama",
+            "query": {
+                "groups": [{
+                    "conditions": [obama_condition]
+                }]
+            },
+        }
+
+        for sc in [SpecialCoverage.objects.create(id=90,
+                                                  name="Uncle Joe",
+                                                  description="Classic Joeseph Biden",
+                                                  query=biden_query),
+                   SpecialCoverage.objects.create(id=91,
+                                                  name="Obama",
+                                                  description="Classic Obama",
+                                                  query=obama_query),
+                   ]:
+            # Manually index this percolator
+            sc._save_percolator()
+
+    def resolve(self, **data):
+        return self.api_client.get(reverse("resolve-special-coverage-list"),
+                                   data=data, format="json")
+
+    def test_found_one(self):
+        # Add some content
+        content = Content.objects.create(
+            id=123,
+            title='a',
+            published=timezone.now() - timedelta(hours=1),
+        )
+        content.tags.add(Tag.objects.create(name='joe-biden'))
+        content.save()
+        Content.search_objects.refresh()
+
+        r = self.resolve(content_id=123)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(1, len(r.data))
+        data = r.data[0]
+        self.assertEqual(90, data['id'])
+        self.assertEqual('Uncle Joe', data['name'])
+
+    def test_found_multiple(self):
+        # Add some content
+        content = Content.objects.create(
+            id=123,
+            title='a',
+            published=timezone.now() - timedelta(hours=1),
+        )
+        content.tags.add(Tag.objects.create(name='joe-biden'))
+        content.tags.add(Tag.objects.create(name='obama'))
+        content.save()
+        Content.search_objects.refresh()
+
+        r = self.resolve(content_id=123)
+        self.assertEqual(r.status_code, 200)
+        self.assertCountEqual([90, 91], [r['id'] for r in r.data])
+
+    def test_no_special_coverage(self):
+        make_content(id=123)
+        r = self.resolve(content_id=123)
+        self.assertEqual(r.status_code, 204)
+
+    def test_invalid_content(self):
+        r = self.resolve(content_id=123)
+        self.assertEqual(r.status_code, 404)
