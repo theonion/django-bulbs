0b58f1a911ddf7584200b556074aa739a2913c4e
Cameron Lowe <cam.m.lowe@gmail.com>
2016-01-26 11:23:52 -0600

    fix dumb admin query

--- bulbs/contributions/admin.py
+++ bulbs/contributions/admin.py
@@ -4,7 +4,16 @@ from .models import FreelanceProfile
 
 
 class FreelanceProfileAdmin(admin.ModelAdmin):
-    fields = ("payroll_name", "contributor", "is_freelance", "is_manager")
+    fields = ("payroll_name", "is_freelance", "is_manager")
+    list_display = ("get_full_name",)
+
+    def get_queryset(self, request):
+        qs = super(FreelanceProfileAdmin, self).get_queryset(request)
+        return qs.distinct()
+
+    def get_full_name(self, obj):
+        return obj.contributor.get_full_name()
+    get_full_name.admin_order_field = 'contributor'
 
 
 admin.site.register(FreelanceProfile, FreelanceProfileAdmin)
