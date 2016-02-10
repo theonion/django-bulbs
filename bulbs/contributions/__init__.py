d5db2dfaeae21c12fb5487fc7baa6689f794c074
Cameron Lowe <cam.m.lowe@gmail.com>
2016-02-10 15:00:58 -0600

    remove app config

--- bulbs/contributions/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = "bulbs.contributions.apps.ContributionsConfig"
--- bulbs/contributions/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContributionsConfig(AppConfig):
-    name = "bulbs.contributions"
-    verbose_name = "Contributions"
-
-    def ready(self):
-        import bulbs.contributions.signals
