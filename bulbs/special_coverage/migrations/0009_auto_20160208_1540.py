b05dbdf8f87b34db060c045fa27ea91846975165
Mike Parent <mparent@umich.edu>
2016-02-16 16:03:23 -0600

    Must grab SpecialCoverage model directly to use custom save() method
    Model obtained from apps.get_model() used generic save() that didn't add to percolator.
    
    https://realpython.com/blog/python/data-migrations/#comment-1843026722

--- bulbs/special_coverage/migrations/0009_auto_20160208_1540.py
+++ bulbs/special_coverage/migrations/0009_auto_20160208_1540.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django.db import migrations
+
+from bulbs.special_coverage.models import SpecialCoverage
 
 
 def save_func(apps, schema_editor):
     # Trigger percolator save logic (new method ALWAYS saves percolator, previous
     # deleted percolator if not active).
-    SpecialCoverage = apps.get_model("special_coverage", 'SpecialCoverage')
     for special in SpecialCoverage.objects.all():
         special.save()
 
