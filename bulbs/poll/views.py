ec2f1824ec3c8b76eb7bed1952661b27fa0d3513
Melissa <melpatenator@gmail.com>
2016-02-19 10:40:30 -0600

    testing ordering filter with rest framework

--- bulbs/poll/views.py
+++ bulbs/poll/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.cache import cache_control
 from django.views.generic.detail import DetailView
+from rest_framework import filters
+from rest_framework import generic
 import json
 
 from bulbs.content.views import BaseContentDetailView
@@ -10,6 +12,8 @@ from bulbs.poll.serializers import PollPublicSerializer
 
 class PollDetailView(BaseContentDetailView):
     model = Poll
+    filter_backends = (filters.OrderingFilter,)
+    ordering_fields= "__all__"
 
 class MergedPollDataView(DetailView):
     model = Poll
