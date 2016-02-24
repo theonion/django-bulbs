d8f711b0f303c379e7bd546972afe7bf33c6176e
Michael Butkovic <michael.butkovic@gmail.com>
2016-02-24 13:58:00 -0600

    start retrieve overwrite

--- bulbs/special_coverage/api.py
+++ bulbs/special_coverage/api.py
@@ -1,13 +1,13 @@
-from rest_framework import filters, routers, viewsets
+from rest_framework import filters, routers, viewsets, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
 
 from .filters import SpecialCoverageFilter
 from .models import SpecialCoverage
-from .serializers import SpecialCoverageSerializer
+from .serializers import SpecialCoverageSerializer, SpecialCoverageLandingSerializer
 
 
 class SpecialCoverageViewSet(viewsets.ModelViewSet):
-    queryset = SpecialCoverage.objects.all()
     serializer_class = SpecialCoverageSerializer
     filter_backends = (
         SpecialCoverageFilter,
@@ -29,6 +29,18 @@ class SpecialCoverageViewSet(viewsets.ModelViewSet):
     )
     paginate_by = 10
     permission_classes = [IsAdminUser]
+
+    def retrieve(self, request, *args, **kwargs):
+        coverage = self.get_object()
+        content = coverage.get_content()
+
+        # get special coverage object and return its .get_content()
+
+        result = SpecialCoverageLandingSerializer(coverage)
+        return Response(result.data, status=status.HTTP_200_OK)
+
+    def get_queryset(self):
+        return SpecialCoverage.objects.all()
 
 api_v1_router = routers.DefaultRouter()
 api_v1_router.register(
