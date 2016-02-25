f677a82af9db4e20301e13d3745cf4b705000c0b
Michael Butkovic <michael.butkovic@gmail.com>
2016-02-25 12:14:39 -0600

    add url, view

--- bulbs/special_coverage/views.py
+++ bulbs/special_coverage/views.py
@@ -1,25 +1,32 @@
-from django.views.generic import DetailView
 from django.http import Http404
+from django.views.generic import DetailView
 from django.shortcuts import get_object_or_404
 
-
 from bulbs.special_coverage.models import SpecialCoverage
+from bulbs.content.views import BaseContentDetailView
 
 
-class SpecialCoverageView(DetailView):
+class SpecialCoverageView(BaseContentDetailView):
+    redirect_correct_path = False
 
     def show_published_only(self):
         return bool("full_preview" not in self.request.GET)
 
     def get_object(self, *args, **kwargs):
         self.special_coverage = get_object_or_404(SpecialCoverage, slug=self.kwargs.get("slug"))
+
         if not self.special_coverage.is_active:
             raise Http404("Special Coverage does not exist.")
-        return self.special_coverage.get_content(published=self.show_published_only()).full()[0]
+
+        qs = self.special_coverage.get_content(published=self.show_published_only()).full()
+        if qs.count() == 0:
+            raise Http404("No Content available in content list")
+
+        return qs[0]
 
     def get_context_data(self, *args, **kwargs):
         context = super(SpecialCoverageView, self).get_context_data()
-        context["content_list"] = self.object.get_content()
+        context["content_list"] = self.special_coverage.get_content()
 
         return context
 
