03c63548c8ab0ed4dac1902ca27c4f87447e31dc
Mike Parent <mparent@umich.edu>
2016-03-03 15:38:29 -0600

    Initial implementation
    Still needs tests

--- bulbs/utils/vault.py
+++ bulbs/utils/vault.py
@@ -11,8 +11,44 @@ from django.conf import settings
 
 log = __import__('logging').getLogger(__name__)
 
+CACHE_SEC = 60
+BACKSTOP_CACHE_SEC = (60 * 60)
+
 
 def read(path):
+    """Primary interface to read data from Vault REST endpoint
+
+    Uses 2 caches:
+        short: Short-term caching to avoid overloading Vault service
+        backstop: Longer-term cache, used as fallback if Vault request fails. Protects against vault service outages.
+    """
+
+    short_key = _make_key('short', path)
+    backstop_key = _make_key('backstop', path)
+
+    result = cache.get(short_key)
+
+    if not result:
+
+        try:
+            result = _read_uncached(path)
+            cache.set(short_key, result, CACHE_SEC=60)
+            cache.set(backstop_key, result, BACKSTOP_CACHE_SEC=60)
+        except VaultRequestError:
+            log.exception('Vault read error: %s', path)
+            # On endpoint failure, try to get value from longer-lasting cache
+            result = cache.get(backstop_key)
+            if not result:
+                raise
+
+    return result
+
+
+def _make_key(*args):
+    return ':'.join(['vault', 'read', CACHE_VERSION] + args])
+
+
+def _read_uncached(path):
     """Read a secret from Vault REST endpoint"""
     url = '{}/{}/{}'.format(settings.VAULT_BASE_URL.rstrip('/'),
                             settings.VAULT_BASE_SECRET_PATH.strip('/'),
