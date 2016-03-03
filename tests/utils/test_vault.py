97edc6ac35971d79b7784a80d4f0a11a43cace99
Mike Parent <mparent@umich.edu>
2016-03-03 16:23:39 -0600

    More testing

--- tests/utils/test_vault.py
+++ tests/utils/test_vault.py
@@ -13,6 +13,9 @@ MEMORY_CACHE = {
     }
 }
 
+TEST_DATA = 'RESPONSE'
+
+
 class VaultReadTestCase(TestCase):
 
     def test_make_key(self):
@@ -23,28 +26,35 @@ class VaultReadTestCase(TestCase):
             cache.clear()
 
             # Saved to backstop cache
-            with patch('bulbs.utils.vault._read_endpoint', return_value='RESPONSE'):
-                self.assertEqual('RESPONSE', read('path'))
+            with patch('bulbs.utils.vault._read_endpoint', return_value=TEST_DATA):
+                self.assertEqual(TEST_DATA, read('path'))
+                # Saved to caches
+                self.assertEqual(TEST_DATA, cache.get('vault:read:1:short:path'))
+                self.assertEqual(TEST_DATA, cache.get('vault:read:1:backstop:path'))
 
             # Loads from backstop cache
             with patch('bulbs.utils.vault._read_endpoint', side_effect=VaultError):
-                self.assertEqual('RESPONSE', read('path'))
+                self.assertEqual(TEST_DATA, read('path'))
 
     def test_backstop_miss(self):
         with patch('bulbs.utils.vault._read_endpoint', side_effect=VaultError):
             with patch('bulbs.utils.vault.logger') as mock_logger:
                 with self.assertRaises(VaultError):
-                    self.assertEqual('RESPONSE', read('path'))
+                    self.assertEqual(TEST_DATA, read('path'))
                     self.assertTrue(mock_logger.exception.call_args)  # Exception logged
 
     def test_cache_miss_then_hit(self):
         with self.settings(CACHES=MEMORY_CACHE):
             cache.clear()
 
-            with patch('bulbs.utils.vault._read_endpoint', return_value='RESPONSE') as mock_endpoint:
+            with patch('bulbs.utils.vault._read_endpoint', return_value=TEST_DATA) as mock_endpoint:
 
-                self.assertEqual('RESPONSE', read('path'))
+                self.assertEqual(TEST_DATA, read('path'))
                 self.assertEqual(1, mock_endpoint.call_count)
 
-                self.assertEqual('RESPONSE', read('path'))
+                # Saved to caches
+                self.assertEqual(TEST_DATA, cache.get('vault:read:1:short:path'))
+                self.assertEqual(TEST_DATA, cache.get('vault:read:1:backstop:path'))
+
+                self.assertEqual(TEST_DATA, read('path'))
                 self.assertEqual(1, mock_endpoint.call_count)
