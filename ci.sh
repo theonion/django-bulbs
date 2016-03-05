a4de2fd5bd36855b683a1abc97bca53919ef9aab
Collin Miller <collintmiller@gmail.com>
2016-03-04 18:01:40 -0600

    touch

--- ci.sh
+++ ci.sh
@@ -3,6 +3,7 @@ set -e
 
 docker-compose -f docker-compose.ci.yml build
 
+
 docker-compose -f ./docker-compose.ci.yml run py2_7_es1_4_4
 docker-compose -f ./docker-compose.ci.yml run py2_7_es1_4_5
 docker-compose -f ./docker-compose.ci.yml run py2_7_es1_7_5
