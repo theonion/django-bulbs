a9b35ceccac7f968f4cda089dfa814b468d34ba6
Michael Butkovic <michael.butkovic@gmail.com>
2016-03-14 15:31:41 -0500

    remove unused stuff

--- tests/videos/test_videos_views.py
+++ tests/videos/test_videos_views.py
@@ -1,11 +1,7 @@
-from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.test import Client
-from django.utils import timezone
 
-from bulbs.utils.test import BaseIndexableTestCase, make_content
-from bulbs.campaigns.models import Campaign
-from bulbs.special_coverage.models import SpecialCoverage
+from bulbs.utils.test import BaseIndexableTestCase
 
 
 class TestVideosViews(BaseIndexableTestCase):
