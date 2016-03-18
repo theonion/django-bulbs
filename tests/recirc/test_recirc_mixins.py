7814a07af1511d400cb8b713ff15babb19a64039
Michael Butkovic <michael.butkovic@gmail.com>
2016-03-18 10:32:26 -0500

    stub out test file

---
+++ tests/recirc/test_recirc_mixins.py
@@ -0,0 +1,13 @@
+from bulbs.utils.test import BaseIndexableTestCase
+
+
+from example.testcontent.models import TestRecircContentObject
+
+
+class TestRericMixins(BaseIndexableTestCase):
+
+    def setUp(self):
+        super(TestRericMixins, self).setUp()
+
+    def test_mixin_model_creation(self):
+        pass
