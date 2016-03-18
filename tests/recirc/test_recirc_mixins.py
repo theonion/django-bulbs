bb692788d9a37f6ec88a4e92f66f3f20dc09ae26
Michael Butkovic <michael.butkovic@gmail.com>
2016-03-18 13:46:27 -0500

    base level test

--- tests/recirc/test_recirc_mixins.py
+++ tests/recirc/test_recirc_mixins.py
@@ -1,5 +1,7 @@
-from bulbs.utils.test import BaseIndexableTestCase
+from django.utils import timezone
 
+from bulbs.utils.test import BaseIndexableTestCase
+from bulbs.content.models import Content, Tag
 
 from example.testcontent.models import TestRecircContentObject
 
@@ -9,5 +11,29 @@ class TestRericMixins(BaseIndexableTestCase):
     def setUp(self):
         super(TestRericMixins, self).setUp()
 
-    def test_mixin_model_creation(self):
-        pass
+        tag_names = (
+            "Cool", "Funny", "Wow", "Amazings"
+        )
+        self.tags = []
+        for name in tag_names:
+            self.tags.append(Tag.objects.create(name=name))
+
+    def test_mixin_query_creation(self):
+        objects = []
+        for tag in self.tags:
+            t = TestRecircContentObject.objects.create(
+                foo="foo",
+                bar="bar"
+            )
+            t.tags.add(tag)
+            t.save()
+            objects.append(t)
+
+        objects[0].query = dict(
+            included_ids=[objects[i].id for i in range(1, len(objects))]
+        )
+        objects[0].save()
+
+        self.assertEqual(objects[0].query, {
+            "included_ids": [objects[i].id for i in range(1, len(objects))]
+        })
