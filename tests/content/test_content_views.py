6624352dfb535ac31faeb835c0d169bb638869a7
Michael Butkovic <michael.butkovic@gmail.com>
2016-03-28 17:24:37 -0500

    fix flaky test

--- tests/content/test_content_views.py
+++ tests/content/test_content_views.py
@@ -67,7 +67,9 @@ class TestContentViews(BaseIndexableTestCase):
             expire_date=expire_date.isoformat()
         )
         uuid = obfuscated_url_info.url_uuid
+
         Content.search_objects.refresh()
+        ObfuscatedUrlInfo.search_objects.refresh()
 
         # attempt to get article via token
         response = self.client.get(reverse("unpublished", kwargs={"token": uuid}))
