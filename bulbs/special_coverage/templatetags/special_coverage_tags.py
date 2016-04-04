ea5c78f1fc9b5e24cabf03a8efbae6bb9fee94fb
Jamie Levinson <jlevinson@theonion.com>
2016-04-04 17:05:17 -0500

    Adjust template

--- bulbs/special_coverage/templatetags/special_coverage_tags.py
+++ bulbs/special_coverage/templatetags/special_coverage_tags.py
@@ -4,6 +4,6 @@ from django.template.loader import render_to_string
 register = template.Library()
 
 
-@register.simple_tag()
-def special_coverage_landing_partial(takes_context=True):
+@register.simple_tag(takes_context=True)
+def special_coverage_landing_partial(context):
     return render_to_string("special_coverage/bulbs_landing.html")
