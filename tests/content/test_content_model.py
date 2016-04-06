dc66f983e9de4940a8f24b1121862d37f8b94392
Cameron Lowe <cam.m.lowe@gmail.com>
2016-04-06 13:00:42 -0500

    testing.

--- tests/content/test_content_model.py
+++ tests/content/test_content_model.py
@@ -1,11 +1,29 @@
 import datetime
 
 from django.utils import timezone
-from bulbs.utils.test import BaseIndexableTestCase
+
 import elasticsearch
 
 from bulbs.content.models import Content
-from bulbs.utils.test import make_content
+from bulbs.utils.test import make_content, BaseIndexableTestCase
+
+from example.testcontent.models import TestContentObj
+
+
+class ContentModelTestCase(BaseIndexableTestCase):
+
+    def setUp(self):
+        super(ContentModelTestCase, self).setUp()
+        self.default_data = {"foo": "Mike sucks."}
+
+    def test_get_template_name_default(self):
+        obj = TestContentObj.objects.create(**self.default_data)
+        self.assertIsNone(obj.get_template_name())
+
+    def test_get_template_name(self):
+        self.default_data.update({"template_choice": 1})
+        obj = TestContentObj.objects.create(**self.default_data)
+        self.assertEqual(obj.get_template_name(), "special_coverage/landing.html")
 
 
 class SerializerTestCase(BaseIndexableTestCase):
