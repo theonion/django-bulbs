af90a03555621facb0854b1897aac8210968d2d0
Andrew Kos <akos123@gmail.com>
2016-04-07 17:04:07 -0500

    added content_tunic_campaign_url tag for building tunic campaign urls

---
+++ tests/content/test_content_templatetags.py
@@ -0,0 +1,37 @@
+from django.template import Context, Template, TemplateSyntaxError
+from django.test import override_settings
+
+from bulbs.utils.test import BaseIndexableTestCase
+
+
+TEST_TUNIC_BACKEND_ROOT = "//my.tunic.url"
+TEST_TUNIC_API_PATH = "/api/path/"
+
+
+class ContentTemplateTagsTestCase(BaseIndexableTestCase):
+
+    def setUp(self):
+        super(ContentTemplateTagsTestCase, self).setUp()
+
+    @override_settings(
+        TUNIC_BACKEND_ROOT=TEST_TUNIC_BACKEND_ROOT,
+        TUNIC_API_PATH=TEST_TUNIC_API_PATH,
+    )
+    def test_content_tunic_campaign_url(self):
+        """Test how template renders with an id."""
+
+        campaign_id = 1
+
+        t = Template("{{% load content %}}{{% content_tunic_campaign_url {} %}}".format(campaign_id))
+        c = Context({})
+        self.assertEquals(t.render(c), "{}{}/{}/public".format(
+            TEST_TUNIC_BACKEND_ROOT,
+            TEST_TUNIC_API_PATH,
+            campaign_id
+        ))
+
+    def test_content_tunic_campaign_url_no_id(self):
+        """Template should error out if no id was given."""
+
+        with self.assertRaises(TemplateSyntaxError):
+            Template("{% load content %}{% content_tunic_campaign_url %}")
