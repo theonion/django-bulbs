376d0f304a7e71bac5ddde6c4a591de4fa0936d7
Andrew Kos <akos123@gmail.com>
2016-04-08 17:11:45 -0500

    fix campaign url

--- bulbs/content/templatetags/content.py
+++ bulbs/content/templatetags/content.py
@@ -1,3 +1,5 @@
+from urlparse import urljoin
+
 from django import template
 from django.conf import settings
 
@@ -7,8 +9,8 @@ register = template.Library()
 
 @register.simple_tag
 def content_tunic_campaign_url(campaign_id):
-    return "{}{}/{}/public".format(
-        settings.TUNIC_BACKEND_ROOT,
+    path = urljoin(
         settings.TUNIC_API_PATH,
-        campaign_id
+        "campaign/{}/public".format(campaign_id)
     )
+    return urljoin(settings.TUNIC_BACKEND_ROOT, path)
