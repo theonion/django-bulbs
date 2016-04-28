0916d458c499925d5d6f8761e11db464f4eb6205
Jamie Levinson <jlevinson@theonion.com>
2016-04-28 12:02:49 -0500

    Add analytics

--- bulbs/recirc/static/recirc/js/series-recirc.js
+++ bulbs/recirc/static/recirc/js/series-recirc.js
@@ -13,7 +13,6 @@ VideoRecircList.prototype.init = function() {
 
 VideoRecircList.prototype.loadVideoRecirc = function() {
   $.getJSON($source, function(data) {
-    console.log(data);
     $.each(data.results.slice(0,$recircCount), function(index, video) {
       $videoTitle = video.title;
       $videoHref = '/v/' + video.id;
@@ -21,6 +20,8 @@ VideoRecircList.prototype.loadVideoRecirc = function() {
       $('<a>',{
         'class' : $videoItem,
         'href' : $videoHref,
+        'data-track-action' : 'Video: Recirc',
+        'data-track-label' : $videoHref,
         'html': $('<figure>',{
           'class' : 'content',
           'html' : '<div class="image"><img src="' + $posterSource + '/16x9/480.jpg"></div>'
