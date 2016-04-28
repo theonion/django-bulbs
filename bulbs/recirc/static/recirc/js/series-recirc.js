c02e1fe5b0c0599cb8df834a1f3a9303628ffd5b
Jamie Levinson <jlevinson@theonion.com>
2016-04-28 17:02:11 -0500

    Refactor js

--- bulbs/recirc/static/recirc/js/series-recirc.js
+++ bulbs/recirc/static/recirc/js/series-recirc.js
@@ -1,9 +1,9 @@
 var VideoRecircList = function() {
-  $videoItem = 'video-item';
   $videoRecircList = $('#video-list');
-  $bettyUrl = $videoRecircList.data('betty-url');
-  $source = $videoRecircList.data('source');
-  $recircCount = $videoRecircList.data('count');
+  this.videoItem = 'video-item';
+  this.bettyUrl = $videoRecircList.data('betty-url');
+  this.source = $videoRecircList.data('source');
+  this.recircCount = $videoRecircList.data('count');
   this.init();
 };
 
@@ -12,21 +12,21 @@ VideoRecircList.prototype.init = function() {
 };
 
 VideoRecircList.prototype.loadVideoRecirc = function() {
-  $.getJSON($source, function(data) {
-    $.each(data.results.slice(0,$recircCount), function(index, video) {
-      $videoTitle = video.title;
-      $videoHref = '/v/' + video.id;
-      $posterSource = $bettyUrl + '/' + video.poster.id
+  $.getJSON(this.source, function(data) {
+    $.each(data.results.slice(0,this.recircCount), function(index, video) {
+      var videoTitle = video.title;
+      var videoHref = '/v/' + video.id;
+      var posterSource = $bettyUrl + '/' + video.poster.id;
       $('<a>',{
-        'class' : $videoItem,
-        'href' : $videoHref,
+        'class' : videoItem,
+        'href' : videoHref,
         'data-track-action' : 'Video: Recirc',
-        'data-track-label' : $videoHref,
+        'data-track-label' : videoHref,
         'html': $('<figure>',{
           'class' : 'content',
-          'html' : '<div class="image"><img src="' + $posterSource + '/16x9/480.jpg"></div>'
+          'html' : '<div class="image"><img src="' + posterSource + '/16x9/480.jpg"></div>'
         }).add($('<p>',{
-          'html' : $videoTitle
+          'html' : videoTitle
         }))
       }).appendTo($videoRecircList);
     });
