3db63110eedb324fd7e0a5bec92932ae0691379c
Mike Parent <mparent@umich.edu>
2016-05-05 14:01:06 -0500

    Merge pull request #174 from theonion/glance-feed-tweaks
    Glance feed tweaks

--- bulbs/feeds/urls.py
+++ bulbs/feeds/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url, patterns
+from django.views.decorators.cache import cache_control
 
 from .views import GlanceFeedViewSet, RSSView, SpecialCoverageRSSView
 
 
 urlpatterns = patterns(
     "",
-    url(r"^rss", RSSView.as_view(), name="rss-feed"),
-    url(r"^sc-rss", SpecialCoverageRSSView.as_view(), name="sc-rss-feed"),
-    url(r"^glance.json$", GlanceFeedViewSet.as_view({'get': 'list'}), name="glance-feed"),
+    url(r"^rss", cache_control(max_age=300)(RSSView.as_view()),
+        name="rss-feed"),
+    url(r"^sc-rss", cache_control(max_age=300)(SpecialCoverageRSSView.as_view()),
+        name="sc-rss-feed"),
+    url(r"^glance.json$", cache_control(max_age=300)(GlanceFeedViewSet.as_view({'get': 'list'})),
+        name="glance-feed"),
 )
