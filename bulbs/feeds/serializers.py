83b21319cef8c82c513171aa85b075887bb36071
Mike Parent <mparent@umich.edu>
2016-05-05 11:42:44 -0500

    Glance Feed: Switch from "feature_media" to "images"

--- bulbs/feeds/serializers.py
+++ bulbs/feeds/serializers.py
@@ -1,29 +1,18 @@
 from rest_framework import serializers
 
 
-class GlanceFeaturedMediaSerializer(serializers.Serializer):
-    type = serializers.CharField()
-    image = serializers.CharField()
-    markup = serializers.CharField()
-
-    def to_representation(self, obj):
-        return {
-            "type": "image",
-            "image": obj.thumbnail.get_crop_url(ratio='16x9'),
-            "markup": "",
-        }
-
-
 class GlanceContentSerializer(serializers.Serializer):
 
     type = serializers.IntegerField(required=True, allow_null=False)
     id = serializers.CharField()
     title = serializers.CharField()
-    link = serializers.CharField()
+    link = serializers.URLField()
     modified = serializers.DateTimeField()
     published = serializers.DateTimeField()
     slug = serializers.CharField()
-    featured_media = GlanceFeaturedMediaSerializer
+    images = serializers.DictField(
+        child=serializers.URLField()
+    )
     authors = serializers.ListField(
         child=serializers.CharField()
     )
@@ -33,13 +22,15 @@ class GlanceContentSerializer(serializers.Serializer):
 
     def to_representation(self, obj):
         return {
-            "type": "post",
-            "id": obj.id,
-            "title": obj.title,
-            "modified": obj.last_modified.isoformat(),
-            "published": obj.published.isoformat(),
-            "slug": obj.slug,
-            "featured_media": GlanceFeaturedMediaSerializer(obj).data,
+            'type': 'post',
+            'id': obj.id,
+            'title': obj.title,
+            'modified': obj.last_modified.isoformat(),
+            'published': obj.published.isoformat(),
+            'slug': obj.slug,
+            'images': {
+                'post-16-9-thumbnail': obj.thumbnail.get_crop_url(ratio='16x9'),
+            },
             'link': self.context['request'].build_absolute_uri(obj.get_absolute_url()),
             # mparent(2016-05-04) TODO: Optional author support
             'authors': ["America's Finest News Source"],
