a3ebd072a3a630426def9c27c8f57c803b6d10e2
Michael Butkovic <michael.butkovic@gmail.com>
2016-05-17 14:13:34 -0500

    rename parser to renderer

---
+++ bulbs/instant_articles/renderer.py
@@ -0,0 +1,70 @@
+class Embed:
+
+    def __init__(self):
+        pass
+
+    def generate_html(self):
+        return html
+
+
+class OnionVideoEmbed():
+
+    def __init__(self, body):
+        pass
+
+
+class InstantArticleRenderer():
+
+    def __init__(self, intermediate):
+        pass
+
+    def generate_body(self, intermediate):
+        body = ""
+        for key, body in intermediate.iteritems():
+            body.append(self.render_item(key, body))
+
+    def render_item(self, key, body):
+        if key == "text":
+            return body
+        elif key == "betty":
+            return self.render_betty(body)
+        elif key == "facebook":
+            return self.render_facebook(body)
+        elif key == "twitter":
+            return self.render_twitter(body)
+        elif key == "instagram":
+            return self.render_instagram(body)
+        elif key == "onion_video":
+            return self.render_onion_video(body)
+        elif key == "vimeo":
+            return self.render_vimeo(body)
+        elif key == "youtube":
+            return self.render_youtube(body)
+        elif key == "soundcloud":
+            return self.render_soundcloud(body)
+        else:
+            raise Exception("Key not implemented")
+
+    def render_betty(self, body):
+        pass
+
+    def render_facebook(self, body):
+        pass
+
+    def render_twitter(self, body):
+        pass
+
+    def render_instagram(self, body):
+        pass
+
+    def render_onion_video(self, body):
+        pass
+
+    def render_vimeo(self, body):
+        pass
+
+    def render_youtube(self, body):
+        pass
+
+    def render_soundcloud(self, body):
+        pass
