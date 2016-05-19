a69e1c339a51812bb32164d4334848c3cc8609d9
Michael Butkovic <michael.butkovic@gmail.com>
2016-05-19 13:34:16 -0500

    use assert equal, use op-social class

--- tests/instant_articles/test_renderer.py
+++ tests/instant_articles/test_renderer.py
@@ -30,7 +30,7 @@ class InstantArticleRendererTests(BaseIndexableTestCase):
         output = self.renderer.render_item(name, data)
         self.assertEqual(
             output.replace('\n', ''),
-            '<figure><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fsarahpalin%2Fposts%2F10154115152888588&width=500" width="500" height="603" scrolling="no" frameborder="0" allowTransparency="true"></iframe></figure>'
+            '<figure class="op-social"><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fsarahpalin%2Fposts%2F10154115152888588&width=500" width="500" height="603" scrolling="no" frameborder="0" allowTransparency="true"></iframe></figure>'
         )
 
     def test_render_imgur(self):
@@ -48,9 +48,9 @@ class InstantArticleRendererTests(BaseIndexableTestCase):
         name, data = block.items()[0]
 
         output = self.renderer.render_item(name, data)
-        self.assertTrue(
+        self.assertEqual(
             output.replace('\n', ''),
-            '<figure><iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="https://www.instagram.com/p/3ewOSHitL2/embed/captioned/?v=7" allowtransparency="true" frameborder="0"></iframe></figure>'
+            '<figure class="op-social"><iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="https://www.instagram.com/p/3ewOSHitL2/embed/captioned/?v=7" allowtransparency="true" frameborder="0"></iframe></figure>'
         )
 
     def test_render_text(self):
@@ -58,7 +58,7 @@ class InstantArticleRendererTests(BaseIndexableTestCase):
         name, data = block.items()[0]
 
         output = self.renderer.render_item(name, data)
-        self.assertTrue(
+        self.assertEqual(
             output.replace('\n', ''),
             '<p>This is a paragraph of text</p>'
         )
@@ -93,7 +93,7 @@ class InstantArticleRendererTests(BaseIndexableTestCase):
         name, data = block.items()[0]
 
         output = self.renderer.render_item(name, data)
-        self.assertTrue(
+        self.assertEqual(
             output.replace('\n', ''),
             '<figure class="op-social"><iframe src="http://onionstudios.com/embed?id=3040" class="onionstudios-playlist" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen="" frameborder="no" width="560" height="315" scrolling="no"></iframe></figure>'
         )
@@ -103,7 +103,7 @@ class InstantArticleRendererTests(BaseIndexableTestCase):
         name, data = block.items()[0]
 
         output = self.renderer.render_item(name, data)
-        self.assertTrue(
+        self.assertEqual(
             output.replace('\n', ''),
             '<figure class="op-social"><iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/119434855%3Fsecret_token%3Ds-Dv1kg&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe></figure>'
         )
@@ -113,7 +113,7 @@ class InstantArticleRendererTests(BaseIndexableTestCase):
         name, data = block.items()[0]
 
         output = self.renderer.render_item(name, data)
-        self.assertTrue(
+        self.assertEqual(
             output.replace('\n', ''),
             '<figure class="op-social"><iframe src="https://player.vimeo.com/video/166544005" width="640" height="360" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></figure>'
         )
