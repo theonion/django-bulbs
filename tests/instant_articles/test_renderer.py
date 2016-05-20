c95d53fbb1c432f865948474c16d0a4f24000385
Michael Butkovic <michael.butkovic@gmail.com>
2016-05-20 12:22:27 -0500

    convert to list

--- tests/instant_articles/test_renderer.py
+++ tests/instant_articles/test_renderer.py
@@ -16,7 +16,7 @@ class InstantArticleRendererTests(TestCase):
     def test_render_betty(self):
         with patch('djbetty.storage.settings.BETTY_IMAGE_URL', '//images.onionstatic.com/onion'):
             block = {"betty": {"image_id": 2349, "caption": "A really good caption"}}
-            name, data = block.items()[0]
+            name, data = list(block.items())[0]
 
             output = self.renderer.render_item(name, data)
             self.assertEqual(
@@ -26,7 +26,7 @@ class InstantArticleRendererTests(TestCase):
 
     def test_render_facebook(self):
         block = {"facebook": {"iframe": '<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fsarahpalin%2Fposts%2F10154115152888588&width=500" width="500" height="603" scrolling="no" frameborder="0" allowTransparency="true"></iframe>'}}
-        name, data = block.items()[0]
+        name, data = list(block.items())[0]
 
         output = self.renderer.render_item(name, data)
         self.assertEqual(
@@ -36,7 +36,7 @@ class InstantArticleRendererTests(TestCase):
 
     def test_render_imgur(self):
         block = {"imgur": {"iframe": '<iframe allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" class="imgur-embed-iframe-pub imgur-embed-iframe-pub-a-K1DxV-true-540" scrolling="no" src="http://imgur.com/a/K1DxV/embed?pub=true&amp;ref=http%3A%2F%2Fstaff.avclub.com%2Fcms%2Fapp%2Fedit%2F236963%2F&amp;w=540" id="imgur-embed-iframe-pub-a-K1DxV"></iframe>'}}
-        name, data = block.items()[0]
+        name, data = list(block.items())[0]
 
         output = self.renderer.render_item(name, data)
         self.assertEqual(
@@ -46,7 +46,7 @@ class InstantArticleRendererTests(TestCase):
 
     def test_render_instagram(self):
         block = {"instagram": {"instagram_id": "3ewOSHitL2"}}
-        name, data = block.items()[0]
+        name, data = list(block.items())[0]
 
         output = self.renderer.render_item(name, data)
         self.assertEqual(
@@ -56,7 +56,7 @@ class InstantArticleRendererTests(TestCase):
 
     def test_render_text(self):
         block = {"text": {"raw": "<p>This is a paragraph of text</p>"}}
-        name, data = block.items()[0]
+        name, data = list(block.items())[0]
 
         output = self.renderer.render_item(name, data)
         self.assertEqual(
@@ -71,7 +71,7 @@ class InstantArticleRendererTests(TestCase):
                  — Trevor Noah (@Trevornoah) <a href="https://twitter.com/Trevornoah/status/583019964556152832">March 31, 2015</a>
             </blockquote>
         '''}}
-        name, data = block.items()[0]
+        name, data = list(block.items())[0]
 
         output = self.renderer.render_item(name, data)
         self.assertTrue(
@@ -91,7 +91,7 @@ class InstantArticleRendererTests(TestCase):
 
     def test_render_onion_video(self):
         block = {'onion_video': {'iframe': '<iframe src="http://onionstudios.com/embed?id=3040" class="onionstudios-playlist" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen="" frameborder="no" width="560" height="315" scrolling="no"></iframe>'}}
-        name, data = block.items()[0]
+        name, data = list(block.items())[0]
 
         output = self.renderer.render_item(name, data)
         self.assertEqual(
@@ -101,7 +101,7 @@ class InstantArticleRendererTests(TestCase):
 
     def test_render_soundcloud(self):
         block = {'soundcloud': {'iframe': '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/119434855%3Fsecret_token%3Ds-Dv1kg&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>'}}
-        name, data = block.items()[0]
+        name, data = list(block.items())[0]
 
         output = self.renderer.render_item(name, data)
         self.assertEqual(
@@ -111,7 +111,7 @@ class InstantArticleRendererTests(TestCase):
 
     def test_render_vimeo(self):
         block = {'vimeo': {'iframe': '<iframe src="https://player.vimeo.com/video/166544005" width="640" height="360" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>'}}
-        name, data = block.items()[0]
+        name, data = list(block.items())[0]
 
         output = self.renderer.render_item(name, data)
         self.assertEqual(
@@ -121,7 +121,7 @@ class InstantArticleRendererTests(TestCase):
 
     def test_render_youtube(self):
         block = {"youtube": {"video_id": "2vnd49"}}
-        name, data = block.items()[0]
+        name, data = list(block.items())[0]
 
         output = self.renderer.render_item(name, data)
         self.assertEqual(
