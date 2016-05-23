019401a7732ecf04fe42547c13948d264820039e
Cameron Lowe <cam.m.lowe@gmail.com>
2016-05-23 15:24:59 -0500

    get all campaigns for tunic.

--- bulbs/utils/tunic.py
+++ bulbs/utils/tunic.py
@@ -18,20 +18,37 @@ class TunicClient(object):
         headers = {
             'Authorization': 'Token {}'.format(settings.TUNIC_REQUEST_TOKEN),
         }
-
-        url_base = settings.TUNIC_STAFF_BACKEND_ROOT
-        # Handle protocol-relative root paths
-        if url_base.startswith('//'):
-            url_base = 'http:' + url_base
-
-        url = urljoin(url_base,
-                      settings.TUNIC_API_PATH.rstrip('/') + '/' + path.lstrip('/'))
-
+        if settings.TUNIC_STAFF_BACKEND_ROOT in path:
+            url = path
+        else:
+            url = self.handle_protocol_relative_paths(path)
         resp = requests.get(url, headers=headers)
         if not resp.ok:
             raise RequestFailure(response=resp)
         return resp
 
+    def handle_protocol_relative_paths(self, path):
+        url_base = settings.TUNIC_STAFF_BACKEND_ROOT
+        if url_base.startswith('//'):
+            url_base = 'http:' + url_base
+        return urljoin(
+            url_base, settings.TUNIC_API_PATH.rstrip('/') + '/' + path.lstrip('/')
+        )
+
+    def get_campaigns(self, filter_active=False):
+        campaigns = {}
+        path = "/campaign/"
+        if filter_active:
+            path += "?active=True"
+        while True:
+            resp = self.get(path).json()
+            campaigns.update({
+                c["id"]: c for c in resp.get("results", {})
+            })
+            path = resp.get("next")
+            if not path:
+                break
+        return campaigns
+
     def get_active_campaigns(self):
-        return {c['id']: c
-                for c in self.get('/campaign/?active=true').json().get('results', {})}
+        return self.get_campaigns(filter_active=True)
--- tests/utils/test_tunic.py
+++ tests/utils/test_tunic.py
@@ -25,6 +25,35 @@ class GetActiveCampaignsTests(TestCase):
                               2: {'id': 2,
                                   'name': 'Another Campaign'}})
 
+    def test_get_all_paginated_campaigns(self):
+        with requests_mock.mock() as mocker:
+            page1 = "http://onion.local/api/v1/campaign/?active=true"
+            page2 = "http://onion.local/api/v1/campaign/?page=2&active=true"
+            mocker.get(page1,
+                       status_code=200,
+                       json={'next': page2,
+                             'results': [{'id': 1,
+                                          'name': 'My Campaign'},
+                                         {'id': 2,
+                                          'name': 'Another Campaign'}]},
+                       headers={'Authorizaton': 'Token 12345'})
+            mocker.get(page2,
+                       status_code=200,
+                       json={'results': [{'id': 3,
+                                          'name': 'More Campaign'},
+                                         {'id': 4,
+                                          'name': 'Enough with the campaigns already!'}]},
+                       headers={'Authorizaton': 'Token 12345'})
+            self.assertEqual(TunicClient().get_active_campaigns(),
+                             {1: {'id': 1,
+                                  'name': 'My Campaign'},
+                              2: {'id': 2,
+                                  'name': 'Another Campaign'},
+                              3: {'id': 3,
+                                  'name': 'More Campaign'},
+                              4: {'id': 4,
+                                  'name': 'Enough with the campaigns already!'}})
+
     def test_backend_error(self):
         with self.assertRaises(RequestFailure):
             with requests_mock.mock() as mocker:
