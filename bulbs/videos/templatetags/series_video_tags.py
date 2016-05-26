4a4951db6f915b687067e08d02838767ccace2ea
Jamie Levinson <jlevinson@theonion.com>
2016-05-26 14:27:48 -0500

    Rename some things

--- bulbs/videos/templatetags/series_video_tags.py
+++ bulbs/videos/templatetags/series_video_tags.py
@@ -4,12 +4,12 @@ register = template.Library()
 
 
 @register.inclusion_tag('videos/series-video-list.html', takes_context=True)
-def video_series_list(context, channel_slug):
+def series_video_list(context, channel_slug):
     context["channel_slug"] = channel_slug
     return context
 
 
-@register.inclusion_tag('videos/series-video-grid.html', takes_context=True)
-def video_series_page(context, series_slug):
+@register.inclusion_tag('videos/series-page.html', takes_context=True)
+def series_video_page(context, series_slug):
     context["series_slug"] = series_slug
     return context
