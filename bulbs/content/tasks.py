b8c8fe2ef77cad1ad7d89e84c706c1de2e07e42e
Michael Butkovic <michael.butkovic@gmail.com>
2016-06-03 10:29:56 -0500

    add time limit, use helper functions

--- bulbs/content/tasks.py
+++ bulbs/content/tasks.py
@@ -65,19 +65,105 @@ def update_feature_type_rates(featuretype_pk):
                 role_id=role.pk)
 
 
-@shared_task(default_retry_delay=5)
+def post_article(content, body):
+    from .models import Content
+
+    logger = logging.getLogger(__name__)
+    fb_page_id = getattr(settings, 'FACEBOOK_PAGE_ID', '')
+    fb_api_url = getattr(settings, 'FACEBOOK_API_BASE_URL', '')
+    fb_access_token = vault.read(settings.FACEBOOK_TOKEN_VAULT_PATH)
+
+    # Post article to instant article API
+    post = requests.post(
+        '{0}/{1}/instant_articles'.format(fb_api_url, fb_page_id),
+        data={
+            'access_token': fb_access_token,
+            'html_source': body,
+            'published': 'true',
+            'development_mode': 'false'
+        })
+
+    if not post.ok:
+        logger.error('''
+            Error in posting Instant Article.\n
+            Content ID: {0}\n
+            IA ID: {1}\n
+            Status Code: {2}'''.format(
+                content.id,
+                content.instant_article_id,
+                post.status_code))
+        return
+
+    # Poll for status of article
+    response = ""
+    while response != "SUCCESS":
+        time.sleep(1)
+
+        status = requests.get('{0}/{1}?access_token={2}'.format(
+            fb_api_url,
+            post.json().get('id'),
+            fb_access_token
+        ))
+
+        # log errors
+        if not status.ok or status.json().get('status') == "ERROR":
+            logger.error('''
+                Error in getting status of Instant Article.\n
+                Content ID: {0}\n
+                IA ID: {1}\n
+                Status Code: {2}'''.format(
+                    content.id,
+                    content.instant_article_id,
+                    status.status_code))
+            return
+
+        response = status.json().get('status')
+
+    # set instant_article_id to response id
+    Content.objects.filter(pk=content.id).update(
+        instant_article_id=status.json().get('id'))
+
+
+def delete_article(content):
+    logger = logging.getLogger(__name__)
+    fb_api_url = getattr(settings, 'FACEBOOK_API_BASE_URL', '')
+    fb_access_token = vault.read(settings.FACEBOOK_TOKEN_VAULT_PATH)
+
+    delete = requests.delete('{0}/{1}?access_token={2}'.format(
+        fb_api_url,
+        content.instant_article_id,
+        fb_access_token
+    ))
+
+    if not delete.ok:
+        logger.error('''
+            Error in deleting Instant Article.\n
+            Content ID: {0}\n
+            IA ID: {1}\n
+            Status Code: {2}'''.format(
+                content.id,
+                content.instant_article_id,
+                delete.status_code))
+    else:
+        status = delete.json().get('success')
+        if bool(status) is not True:
+            logger.error('''
+                Error in deleting Instant Article.\n
+                Content ID: {0}\n
+                IA ID: {1}\n
+                Error: {2}'''.format(
+                    content.id,
+                    content.instant_article_id,
+                    delete.json()))
+
+
+@shared_task(default_retry_delay=5, time_limit=300)
 def post_to_instant_articles_api(content_pk):
     from .models import Content
 
     if getattr(settings, 'FACEBOOK_API_ENV', '').lower() == 'production':
-        logger = logging.getLogger(__name__)
-
         content = Content.objects.get(pk=content_pk)
         feature_type = getattr(content, 'feature_type', None)
-
-        fb_page_id = getattr(settings, 'FACEBOOK_PAGE_ID', '')
-        fb_api_url = getattr(settings, 'FACEBOOK_API_BASE_URL', '')
-        fb_access_token = vault.read(settings.FACEBOOK_TOKEN_VAULT_PATH)
 
         # if feature type is IA approved & content is published
         if feature_type and feature_type.instant_article and content.is_published:
@@ -98,82 +184,9 @@ def post_to_instant_articles_api(content_pk):
                     'instant_article/base_instant_article.html', context
                 )
 
-            # Post article to instant article API
-            post = requests.post(
-                '{0}/{1}/instant_articles'.format(fb_api_url, fb_page_id),
-                data={
-                    'access_token': fb_access_token,
-                    'html_source': source,
-                    'published': 'true',
-                    'development_mode': 'false'
-                })
-
-            if not post.ok:
-                logger.error('''
-                    Error in posting Instant Article.\n
-                    Content ID: {0}\n
-                    IA ID: {1}\n
-                    Status Code: {2}'''.format(
-                        content.id,
-                        content.instant_article_id,
-                        post.status_code))
-                return
-
-            # Poll for status of article
-            response = ""
-            while response != "SUCCESS":
-                time.sleep(1)
-
-                status = requests.get('{0}/{1}?access_token={2}'.format(
-                    fb_api_url,
-                    post.json().get('id'),
-                    fb_access_token
-                ))
-
-                # log errors
-                if not status.ok or status.json().get('status') == "ERROR":
-                    logger.error('''
-                        Error in getting status of Instant Article.\n
-                        Content ID: {0}\n
-                        IA ID: {1}\n
-                        Status Code: {2}'''.format(
-                            content.id,
-                            content.instant_article_id,
-                            status.status_code))
-                    return
-
-                response = status.json().get('status')
-
-            # set instant_article_id to response id
-            Content.objects.filter(pk=content_pk).update(
-                instant_article_id=status.json().get('id'))
+            post_article(content, source)
 
         # if article is being unpublished, delete it from IA API
         elif (not content.is_published and
               content.instant_article_id):
-            delete = requests.delete('{0}/{1}?access_token={2}'.format(
-                fb_api_url,
-                content.instant_article_id,
-                fb_access_token
-            ))
-
-            if not delete.ok:
-                logger.error('''
-                    Error in deleting Instant Article.\n
-                    Content ID: {0}\n
-                    IA ID: {1}\n
-                    Status Code: {2}'''.format(
-                        content.id,
-                        content.instant_article_id,
-                        delete.status_code))
-            else:
-                status = delete.json().get('success')
-                if bool(status) is not True:
-                    logger.error('''
-                        Error in deleting Instant Article.\n
-                        Content ID: {0}\n
-                        IA ID: {1}\n
-                        Error: {2}'''.format(
-                            content.id,
-                            content.instant_article_id,
-                            delete.json()))
+            delete_article(content)
