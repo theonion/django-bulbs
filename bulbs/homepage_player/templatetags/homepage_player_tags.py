f1212a74c564bcdb9635cbabbf87bf7963ec478e
Collin Miller (Zardulist) <collintmiller@gmail.com>
2016-06-09 15:25:22 -0500

    homepage_player spike

--- bulbs/homepage_player/templatetags/homepage_player_tags.py
+++ bulbs/homepage_player/templatetags/homepage_player_tags.py
@@ -1,9 +1,15 @@
 from django import template
+from bulbs.utils.methods import get_overridable_template_name
 
 register = template.Library()
 
+template_name = get_overridable_template_name(
+        'homepage_player/bulbs_homepage_player.html',
+        'homepage_player/homepage_player_override.html',
+        )
 
-@register.inclusion_tag('homepage_player/bulbs_homepage_player.html', takes_context=True)
+
+@register.inclusion_tag(template_name, takes_context=True)
 def homepage_player_partial(context, videos=[]):
     context['videos'] = videos
     return context
