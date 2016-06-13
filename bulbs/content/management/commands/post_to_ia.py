1e28963827becceb42909409a859296945694f35
Michael Butkovic <michael.butkovic@gmail.com>
2016-06-13 12:50:14 -0500

    start post to ia script

---
+++ bulbs/content/management/commands/post_to_ia.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+
+from bulbs.content.models import Content, FeatureType
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        return
