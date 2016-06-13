d5c9baeeaf6a5d649d48bc36b4a3d934050b1bed
Michael Butkovic <michael.butkovic@gmail.com>
2016-06-13 14:57:43 -0500

    add command

--- bulbs/content/management/commands/post_to_ia.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from bulbs.content.models import Content, FeatureType
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        return
