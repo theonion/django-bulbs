76f8c3db71f09fee5737d57f36794b7bb9833e19
Mike Parent <mparent@umich.edu>
2016-06-15 15:55:03 -0500

    Backout AppConfig changes
    This doesn't solve duplicate app name problems, b/c it breaks bulbs migrations

--- bulbs/recirc/apps.py
+++
@@ -1,8 +0,0 @@
-import django.apps
-
-
-class AppConfig(django.apps.AppConfig):
-    # Full Python path to the application
-    name = 'bulbs.recirc'
-    # Unique label (to avoid conflicts with property apps)
-    label = 'bulbs_recirc'
