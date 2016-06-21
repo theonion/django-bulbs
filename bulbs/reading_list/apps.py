732339d25c3b1aa77701cb17a73b6e87fc04673a
Cameron Lowe <cam.m.lowe@gmail.com>
2016-06-21 15:12:14 -0500

    remove apps.py

--- bulbs/reading_list/apps.py
+++
@@ -1,8 +0,0 @@
-import django.apps
-
-
-class AppConfig(django.apps.AppConfig):
-    # Full Python path to the application
-    name = 'bulbs.reading_list'
-    # Unique label (to avoid conflicts with property apps)
-    label = 'bulbs_reading_list'
