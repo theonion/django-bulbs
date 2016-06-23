72351fb1e385feb444bce95c98abc19c337beed3
Michael Butkovic <michael.butkovic@gmail.com>
2016-06-23 17:08:31 -0500

    check in page model

--- bulbs/page/models.py
+++ bulbs/page/models.py
@@ -1,3 +1,19 @@
+from django_enumfield import enum
+from elasticsearch_dsl import field
+
 from django.db import models
 
-# Create your models here.
+from djes.models import Indexable
+
+from .enum import Status, TemplateType
+
+
+class Page(Indexable):
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    notes = models.TextField()
+    status = enum.EnumField(Status)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    template_type = enum.EnumField(TemplateType)
+    tunic_campaign_id = models.IntegerField(blank=True, null=True, default=None)
