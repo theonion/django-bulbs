20466cb7c9c820692df4efac69c2ae3ea99f8a96
Jamie Levinson <jlevinson@theonion.com>
2016-07-14 11:27:36 -0500

    Setting up share tools

--- bulbs/special_coverage/templatetags/special_coverage_tags.py
+++ bulbs/special_coverage/templatetags/special_coverage_tags.py
@@ -4,7 +4,8 @@ register = template.Library()
 
 
 @register.inclusion_tag('special_coverage/bulbs_sc_landing.html', takes_context=True)
-def special_coverage_landing_partial(context, twitter_handle, share_message):
+def special_coverage_landing_partial(context, twitter_handle, share_message, share_style):
     context['twitter_handle'] = twitter_handle
     context['share_message'] = share_message
+    context['share_style'] = share_style
     return context
