7e574693b3e7983560ffc1b2c4014c7c9d76f9e0
Michael Butkovic <michael.butkovic@gmail.com>
2016-07-15 10:14:44 -0500

    name

--- bulbs/super_features/serializers.py
+++ bulbs/super_features/serializers.py
@@ -8,10 +8,10 @@ from bulbs.super_features.utils import get_data_serializer
 class BaseSuperFeatureDataField(serializers.Field):
 
     def to_internal_value(self, data):
-        serializer = get_data_serializer(
+        serializer_class = get_data_serializer(
             self.parent.initial_data.get("superfeature_type")
         )
-        return serializer().to_internal_value(data)
+        return serializer_class().to_internal_value(data)
 
     def to_representation(self, obj):
         serializer_class = get_data_serializer(
