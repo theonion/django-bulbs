7786e1a936bbc0159f05a8aa00afacfa8a45acd1
Michael Butkovic <michael.butkovic@gmail.com>
2016-07-18 10:35:18 -0500

    separate module from name

--- bulbs/super_features/utils.py
+++ bulbs/super_features/utils.py
@@ -1,3 +1,5 @@
+from importlib import import_module
+
 from django.apps import apps
 from django.conf import settings
 
@@ -25,27 +27,39 @@ def get_superfeature_model():
 
 
 def get_superfeature_serializer():
+    module = getattr(settings, "BULBS_SUPERFEATURE_SERIALIZER_MODULE", None)
     serializer = getattr(settings, "BULBS_SUPERFEATURE_SERIALIZER", None)
-    if serializer is None:
+    if serializer is None or module is None:
         from bulbs.super_features.serializers import BaseSuperFeatureSerializer
         return BaseSuperFeatureSerializer
-    return eval(serializer)
+    else:
+        # NOTE: import serializer module & then evaluate based on serializer name
+        mod = import_module(module)  # NOQA
+        return eval("{0}.{1}".format('mod', serializer))
 
 
 def get_superfeature_partial_serializer():
+    module = getattr(settings, "BULBS_SUPERFEATURE_SERIALIZER_MODULE", None)
     serializer = getattr(settings, "BULBS_SUPERFEATURE_PARTIAL_SERIALIZER", None)
-    if serializer is None:
+    if serializer is None or module is None:
         from bulbs.super_features.serializers import BaseSuperFeaturePartialSerializer
         return BaseSuperFeaturePartialSerializer
-    return eval(serializer)
+    else:
+        # NOTE: import serializer module & then evaluate based on serializer name
+        mod = import_module(module)  # NOQA
+        return eval("{0}.{1}".format('mod', serializer))
 
 
 def get_superfeature_metadata():
+    module = getattr(settings, "BULBS_SUPERFEATURE_METADATA_MODULE", None)
     metadata = getattr(settings, "BULBS_SUPERFEATURE_METADATA", None)
-    if metadata is None:
+    if metadata is None or module is None:
         from bulbs.super_features.metadata import BaseSuperFeatureMetadata
         return BaseSuperFeatureMetadata
-    return eval(metadata)
+    else:
+        # NOTE: import metadata module & then evaluate based on metadata name
+        mod = import_module(module)  # NOQA
+        return eval("{0}.{1}".format('mod', metadata))
 
 
 def get_superfeature_choices():
