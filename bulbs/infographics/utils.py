945be851181acde0e6dc8861a623ec4dc56a556f
Cameron Lowe <cam.m.lowe@gmail.com>
2016-07-19 15:31:14 -0500

    Load infographic metadata

--- bulbs/infographics/utils.py
+++ bulbs/infographics/utils.py
@@ -1,8 +1,23 @@
+from importlib import import_module
+
+from django.conf import settings
+
 from .enum import InfographicType
 from .data_serializers import (
     ComparisonSerializer, ListInfographicDataSerializer, ProConSerializer,
     StrongSideWeakSideSerializer, TimelineSerializer
 )
+
+
+def get_infographics_serializer():
+    module = getattr(settings, "INFOGRAPHICS_MODULE", None)
+    serializer = getattr(settings, "INFOGRAPHICS_SERIALIZER", None)
+    if serializer is None or module is None:
+        from .serializers import InfographicSerializer
+        return InfographicSerializer
+    else:
+        mod = import_module(module)
+        return getattr(mod, serializer)
 
 
 def get_data_serializer(infographic_type):
