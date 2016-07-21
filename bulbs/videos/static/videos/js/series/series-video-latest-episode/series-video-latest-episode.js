4eaaf43608e8a557ed31a4f7363c907927f0d80c
Melissa <melpatenator@gmail.com>
2016-07-21 13:33:17 -0500

    add latest video description to the mix

--- bulbs/videos/static/videos/js/series/series-video-latest-episode/series-video-latest-episode.js
+++ bulbs/videos/static/videos/js/series/series-video-latest-episode/series-video-latest-episode.js
@@ -2,6 +2,7 @@ LatestEpisode = function(videos) {
 
   this.$videoPlayer = $('bulbs-video');
   this.$latestVideoTitle = $('#latest-video-title');
+  this.$latestVideoDescription = $('#latest-video-description');
   this.$videohubBaseUrl = this.$videoPlayer.data('videohub-base');
   this.latestEpisode = videos[0].id + '.json';
   this.latestEpisodeHref = '/v/' + videos[0].id;
@@ -15,6 +16,11 @@ LatestEpisode = function(videos) {
       'html' : '<h2>' + videos[0].title + '</h2>'
 
   }).appendTo(this.$latestVideoTitle);
+
+  if (videos[0].description) {
+    $('<p>' +  videos[0].description + '</p>')
+      .appendTo(this.$latestVideoDescription);
+  }
 
   if (typeof this.$videohubBaseUrl === 'undefined') {
     throw Error('LatestEpisode requires bulbs-video to have a videohub base url.');
--- bulbs/videos/static/videos/js/series/series-video-latest-episode/series-video-latest-episode.test.js
+++ bulbs/videos/static/videos/js/series/series-video-latest-episode/series-video-latest-episode.test.js
@@ -2,7 +2,12 @@ describe('LatestEpisode', function() {
 
   var LatestEpisode = require('./series-video-latest-episode');
   var videos = [
-    { id: 5400, title: 'A Show About Music', poster: { id: 1234 } },
+    {
+      id: 5400,
+      title: 'A Show About Music',
+      poster: { id: 1234 },
+      description: 'it sucks'
+    },
     { id: 6000, title: 'A Show About Movies', poster: { id: 1235 } }
   ];
   var videoPlayer;
@@ -18,6 +23,8 @@ describe('LatestEpisode', function() {
       videoPlayer = $('<bulbs-video data-videohub-base="' + videohubBaseUrl + '">');
 
       $('body').append(videoPlayer);
+      $('body').append('<div id="latest-video-title"></div>');
+      $('body').append('<div id="latest-video-description"></div>');
 
       latestEpisode = new LatestEpisode(videos);
     });
@@ -26,6 +33,15 @@ describe('LatestEpisode', function() {
       expect($(videoPlayer).attr('src')).to.equal(videohubBaseUrl + '/video/' + videos[0].id + '.json');
     });
 
+    it('appends the latest video title', function() {
+      var videoTitle = $('#latest-video-title h2').html()
+      expect(videoTitle).to.equal("A Show About Music");
+    });
+
+    it('appends the latest video description', function() {
+      var videoDescription = $('#latest-video-description p').html()
+      expect(videoDescription).to.equal("it sucks");
+    });
   });
 
   context('should fail when', function () {
