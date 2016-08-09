f25a01eba448ab927877d828eb3c05cf01a5fe87
Chris Sprehe <chris.sprehe@gmail.com>
2016-08-09 16:46:46 -0500

    Bulbs-carousel on series detail page if onion

--- bulbs/videos/static/videos/js/series/series-video-grid/series-video-grid.js
+++ bulbs/videos/static/videos/js/series/series-video-grid/series-video-grid.js
@@ -1,6 +1,7 @@
 SeriesVideoGrid = function (videos, selector) {
 
   this.$seriesGrid = $(selector || '#series-video-list');
+  this.$carousel = $('bulbs-carousel-slider bulbs-carousel-track');
   this.bettyUrl = this.$seriesGrid.data('bettyUrl');
 
   if (typeof this.bettyUrl === 'undefined') {
@@ -15,19 +16,28 @@ SeriesVideoGrid = function (videos, selector) {
     var videoTitle = video.title;
     var videoHref = '/v/' + video.id;
     var posterSource = that.bettyUrl + '/' + video.poster.id + '/16x9/480.jpg';
-    $('<a>',{
-        'class' : 'video-item',
-        'href' : videoHref,
-        'data-track-action' : 'Single Series: Episodes',
-        'data-track-label' : videoHref,
-        'html': $('<figure>',{
-          'class' : 'content',
-          'html' : '<div class="image"><bulbs-video-play-button></bulbs-video-play-button><img src="' + posterSource + '"></div>',
-        }).add($('<p>',{
-          'html' : videoTitle
-        }))
-      }).appendTo(that.$seriesGrid);
+
+    var anchor = $('<a>',{
+      'class' : 'video-item',
+      'href' : videoHref,
+      'data-track-action' : 'Single Series: Episodes',
+      'data-track-label' : videoHref,
+      'html': $('<figure>',{
+        'class' : 'content',
+        'html' : '<div class="image"><bulbs-video-play-button></bulbs-video-play-button><img src="' + posterSource + '"></div>',
+      }).add($('<p>',{
+        'html' : videoTitle
+      }))
     });
+
+    if (that.$seriesGrid.length > 0) {
+      anchor.appendTo(that.$seriesGrid);
+    }
+
+    if (that.$carousel.length > 0) {
+      $('<bulbs-carousel-item>').html(anchor).appendTo(that.$carousel);
+    }
+  });
 };
 
 module.exports = SeriesVideoGrid;
